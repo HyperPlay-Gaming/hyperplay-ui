c2ae04d54b9a478d52a1a99500e73d56d1027700
Brett <27568879+BrettCleary@users.noreply.github.com>
2023-02-08 15:31:14 -0800

    [U] Toasts (#3)
    * add svgs
    
    * Implement toasts

--- packages/ui/src/assets/images/index.tsx
+++ packages/ui/src/assets/images/index.tsx
@@ -1,7 +1,15 @@
 import { ReactComponent as TxnPending } from './TxnPending.svg'
 import { ReactComponent as CloseButton } from './CloseButton.svg'
+import { ReactComponent as TxnAlert } from './TxnAlert.svg'
+import { ReactComponent as TxnSubmitted } from './TxnSubmitted.svg'
+import { ReactComponent as TxnSuccess } from './TxnSuccess.svg'
+import { ReactComponent as TxnError } from './TxnError.svg'
 
 export {
   TxnPending,
-  CloseButton
+  CloseButton,
+  TxnAlert,
+  TxnSubmitted,
+  TxnSuccess,
+  TxnError
 }
