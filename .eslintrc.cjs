c9f2a2619224ab8e9674a56742de019e626006ea
Maximiliaan van Dijk (hellowodl) <max@maxilibrium.com>
2023-03-28 23:40:01 -0600

    fix: linting configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,63 @@
 // only lint files in the src directory
 
 module.exports = {
-  ...require('@hyperplay/eslintconfig'),
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: ['react', '@typescript-eslint', 'import'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-return-await': 'off',
+    '@typescript-eslint/return-await': 'warn',
+    'react/no-unknown-property': [
+      'error',
+      { ignore: ['partition', 'allowpopups', 'useragent'] }
+    ],
+    '@typescript-eslint/promise-function-async': [
+      'error',
+      {
+        allowedPromiseNames: ['Thenable'],
+        checkArrowFunctions: true,
+        checkFunctionDeclarations: true,
+        checkFunctionExpressions: true,
+        checkMethodDeclarations: true
+      }
+    ],
+    complexity: 'off',
+    eqeqeq: ['error', 'always'],
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true }
+    ],
+    'import/no-duplicates': ['error']
+  },
   ignorePatterns: ['!src', 'vite.config.ts', '.eslintrc.cjs'],
   parserOptions: {
     project: './tsconfig.json',
