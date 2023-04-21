aef5564b8b2c6421f0608e265381490dde457bd9
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-04-20 17:22:41 -0700

    remove proprietary fonts

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -4,12 +4,6 @@ import 'react-loading-skeleton/dist/skeleton.css'
 import '../src/fonts.css'
 import '../src/index.scss'
 
-try {
-  import('@hyperplay/proprietary-fonts')
-} catch (e) {
-  console.warn('Proprietary fonts could not be loaded')
-}
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
