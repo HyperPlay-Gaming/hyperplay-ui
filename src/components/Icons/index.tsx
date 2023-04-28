d60793e15100020895f7f0de9ee5e3d46017824a
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-04-27 19:03:46 -0700

    update icons story, add down arrow svg

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -2,37 +2,35 @@ import React from 'react'
 
 import * as IconImages from '@/assets/images'
 
+const IconFill: { [key: string]: string } = {
+  DownloadIcon: '#FFFFFF',
+  PlayIcon: '#FFFFFF',
+  PauseIcon: '#FFFFFF',
+  Checkmark: '#FFFFFF',
+  DownArrow: '#FFFFFF'
+}
+
 export default function Icons() {
+  function getAllIcons() {
+    const icons = []
+    const allImages = IconImages as {
+      [key: string]: React.FunctionComponent<React.SVGAttributes<SVGElement>>
+    }
+    for (const key of Object.keys(allImages)) {
+      const props: { width: string; height: string; fill?: string } = {
+        width: '36px',
+        height: '36px'
+      }
+      if (Object.hasOwn(IconFill, key)) {
+        props.fill = IconFill[key]
+      }
+      icons.push(allImages[key](props))
+    }
+    return icons
+  }
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-      <IconImages.TxnPending />
-      <IconImages.CloseButton />
-      <IconImages.TxnAlert />
-      <IconImages.TxnSubmitted />
-      <IconImages.TxnSuccess />
-      <IconImages.TxnError />
-      <IconImages.ChevronLeft />
-      <IconImages.ChevronRight />
-      <IconImages.ArrowLeft />
-      <IconImages.HyperplayStoreIcon />
-      <IconImages.LinuxIcon />
-      <IconImages.MacOSIcon />
-      <IconImages.WindowsIcon />
-      <IconImages.SteamDeckIcon />
-      <IconImages.WebIcon />
-      <IconImages.BurgerClosedIcon />
-      <IconImages.BurgerOpenIcon />
-      <IconImages.MobileHpLogo />
-      <IconImages.WarningIcon />
-      <IconImages.RightArrow />
-      <IconImages.DownloadIcon fill="#FFFFFF" />
-      <IconImages.PlayIcon fill="#FFFFFF" />
-      <IconImages.XCircle />
-      <IconImages.PauseIcon fill="#FFFFFF" />
-      <IconImages.Checkmark
-        fill="#FFFFFF"
-        style={{ width: '24px', height: '24px' }}
-      />
+      <>{getAllIcons()}</>
     </div>
   )
 }
--- src/global.d.ts
+++ src/global.d.ts
@@ -7,3 +7,9 @@ declare module '*.module.scss' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
+
+declare module '*.svg' {
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGAttributes<SVGElement>
+  >
+}
