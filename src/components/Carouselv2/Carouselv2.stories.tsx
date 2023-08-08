47c4071a790b4f1038df067ced329887c3b39043
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-08-07 21:32:11 -0700

    add youtube videos on carousel

--- src/components/Carouselv2/Carouselv2.stories.tsx
+++ src/components/Carouselv2/Carouselv2.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
 import dtCover from '@/assets/DarkThroneLandscape.png?url'
 import onisCover from '@/assets/OnisQuestLandscape.png?url'
@@ -12,14 +13,50 @@ export default {
 }
 
 const items: SlideData[] = [
-  { img: <img src={pgCover} />, title: 'Phantom Galaxies' },
-  { img: <img src={dtCover} />, title: 'Dark Throne' },
-  { img: <img src={wakeCover} />, title: 'The Wake' },
-  { img: <img src={onisCover} />, title: 'Onis Quest' }
+  { slideElement: <img src={pgCover} />, title: 'Phantom Galaxies' },
+  { slideElement: <img src={dtCover} />, title: 'Dark Throne' },
+  { slideElement: <img src={wakeCover} />, title: 'The Wake' },
+  { slideElement: <img src={onisCover} />, title: 'Onis Quest' }
 ]
 
 export const Default = () => (
   <div style={{ maxWidth: 1080, maxHeight: 400 }}>
-    <Carouselv2 items={items}></Carouselv2>
+    <Carouselv2 items={items} autoplayDelayInMs={6000}></Carouselv2>
   </div>
 )
+
+const itemsWithVideo: SlideData[] = [
+  {
+    slideElement: (
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=bGzW-ps-_vc"
+        width={'100%'}
+        height={'100%'}
+        onPlay={() => console.log('play called')}
+        onClickPreview={() => console.log('preview called')}
+        controls={true}
+      />
+    ),
+    title: '',
+    thumbnail: (
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=bGzW-ps-_vc"
+        width={'100%'}
+        height={'100%'}
+        light={true}
+        playIcon={<></>}
+        style={{ pointerEvents: 'none' }}
+      />
+    ),
+    disableGradient: true
+  },
+  ...items
+]
+
+export const WithYouTubeVideos = () => {
+  return (
+    <div style={{ maxWidth: 1080, maxHeight: 400 }}>
+      <Carouselv2 items={itemsWithVideo} autoplayDelayInMs={6000}></Carouselv2>
+    </div>
+  )
+}
--- src/components/Carouselv2/index.tsx
+++ src/components/Carouselv2/index.tsx
@@ -9,22 +9,28 @@ import styles from './index.module.scss'
 
 export interface SlideData {
   title: string
-  img: JSX.Element
+  slideElement: JSX.Element
+  thumbnail?: JSX.Element
+  disableGradient?: boolean
 }
 
 export interface Carouselv2Props {
   items: SlideData[]
+  autoplayDelayInMs: number
 }
 
 const Carouselv2 = (props: Carouselv2Props) => {
   const [activeIndex, setActiveIndex] = useState(0)
-  const autoplay = useRef(Autoplay({ delay: 6000 }))
+  const autoplay = useRef(Autoplay({ delay: props.autoplayDelayInMs }))
   const [emblaApiRef, setEmblaApiRef] = useState<EmblaCarouselType>()
 
   function getSlides() {
     return props.items.map((item) => (
-      <Carousel.Slide key={item.title}>
-        {item.img}
+      <Carousel.Slide
+        key={item.title}
+        id={item.disableGradient ? 'disableGradient' : 'enableGradient'}
+      >
+        {item.slideElement}
         <h1 className={styles.title}>{item.title}</h1>
       </Carousel.Slide>
     ))
@@ -48,7 +54,9 @@ const Carouselv2 = (props: Carouselv2Props) => {
       </Carousel>
       <div className={styles.controller}>
         <Controller
-          images={props.items.map(({ img }) => img)}
+          images={props.items.map(({ slideElement, thumbnail }) =>
+            thumbnail ? thumbnail : slideElement
+          )}
           onChange={(index) => emblaApiRef?.scrollTo(index)}
           activeIndex={activeIndex}
         />
