e7744c5b6216fa4ffa0e0c011fdab1618fa5cfb0
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2023-08-31 01:48:53 -0400

    refactor(toggle): use correct type (#63)

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,9 +1,9 @@
-import React, { HTMLAttributes, PropsWithChildren } from 'react'
+import React, { InputHTMLAttributes, PropsWithChildren } from 'react'
 
 import styles from './Toggle.module.scss'
 
 export interface ToggleProps
-  extends PropsWithChildren<HTMLAttributes<HTMLInputElement>> {
+  extends PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> {
   disabled?: boolean
   labelPosition?: 'left' | 'right'
 }
