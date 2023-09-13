8b16da7d3bdd706a4b7fa9463f7267e33ddc4e15
Andrew James <andrewpjames87@gmail.com>
2023-09-13 08:18:18 -0600

    chore: add disabled state to AchievementCard docs

--- src/components/AchievementCard/AchievementCard.stories.tsx
+++ src/components/AchievementCard/AchievementCard.stories.tsx
@@ -77,3 +77,17 @@ export const NegativeAchievements = () => (
     />
   </Flex>
 )
+export const Disabled = () => (
+  <Flex maw={300}>
+    <AchievementCard
+      image="https://i.imgur.com/Cij5vdL.png"
+      title="Star Wars: Knights of the Old Republic"
+      mintedAchievementsCount={5}
+      mintableAchievementsCount={10}
+      totalAchievementsCount={-5}
+      data-testid="1"
+      infoText="Information about progress."
+      ctaProps={{ disabled: true }}
+    />
+  </Flex>
+)
