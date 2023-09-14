4cd4d46dd047e345539ebd1100c2ee75599adf65
Andrew James <andrewpjames87@gmail.com>
2023-09-14 15:30:41 -0600

    chore: cleanup for pr review

--- src/components/AchievementProgress/AchievementProgress.stories.tsx
+++ src/components/AchievementProgress/AchievementProgress.stories.tsx
@@ -2,17 +2,25 @@
 import React from 'react'
 
 import AchievementProgress from '.'
+import ProgressKey from './components/ProgressKey'
 
 export default {
   title: 'AchievementProgress',
   component: AchievementProgress
 }
 
-export const Default = () => (
+export const Progress = () => (
   <AchievementProgress
     safeMintedCount={5}
     safeTotalCount={20}
     mintedProgress={10}
     mintableProgress={20}
   />
+)
+
+export const ProgressKeyRow = () => (
+  <ProgressKey safeMintedCount={5} safeTotalCount={20} />
+)
+export const ProgressKeyColumn = () => (
+  <ProgressKey safeMintedCount={5} safeTotalCount={20} direction="column" />
 )
