3c6814dade4c05ab8ab4c30c3d374609ea89c5ef
Charlie Blackstock <charlieblackstock@gmail.com>
2023-09-14 10:44:20 -0600

    feat: add store row component (#71)
    * feat: add store row component
    
    * fix: little clean up
    
    * fix: add way to pass in props to store row
    
    * fix: color on svg fills
    
    * fix: change fill on icons
    
    * fix: prettier
    
    ---------
    
    Co-authored-by: Andrew James <andrewpjames87@gmail.com>

---
+++ src/components/StoreRow/StoreRow.stories.tsx
@@ -0,0 +1,26 @@
+import { Flex } from '@mantine/core'
+
+import { Eye } from '@/assets/images'
+
+import StoreRow from '.'
+
+export default {
+  title: 'Store Row',
+  component: StoreRow
+}
+
+export const Default = () => {
+  return (
+    <Flex direction="column" gap="8px">
+      <StoreRow store="hyperplay" secondaryText="10 Games">
+        <Eye />
+      </StoreRow>
+      <StoreRow store="epic" secondaryText="10 Games">
+        <p>Link Store</p>
+      </StoreRow>
+      <StoreRow store="steam" secondaryText="10 Games">
+        <p>anything else we want</p>
+      </StoreRow>
+    </Flex>
+  )
+}
