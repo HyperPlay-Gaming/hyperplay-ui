7b887cd5d240590b820fc93847496e895b483f7f
Andrew James <andrewpjames87@gmail.com>
2023-09-15 09:51:20 -0600

    chore: display progress key as its own story

--- src/components/AchievementProgress/AchievementProgress.stories.tsx
+++ src/components/AchievementProgress/AchievementProgress.stories.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 
 import AchievementProgress from '.'
-import ProgressKey from './components/ProgressKey'
 
 export default {
   title: 'AchievementProgress',
@@ -16,11 +15,4 @@ export const Progress = () => (
     mintedProgress={10}
     mintableProgress={20}
   />
-)
-
-export const ProgressKeyRow = () => (
-  <ProgressKey safeMintedCount={5} safeTotalCount={20} />
-)
-export const ProgressKeyColumn = () => (
-  <ProgressKey safeMintedCount={5} safeTotalCount={20} direction="column" />
 )
