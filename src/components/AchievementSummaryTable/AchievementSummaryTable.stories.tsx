43186d56ff72290a0a4ee6d97f7ef18322277b98
Andrew James <andrewpjames87@gmail.com>
2023-09-15 15:46:08 -0600

    feat: add pagination and sort functionality

--- src/components/AchievementSummaryTable/AchievementSummaryTable.stories.tsx
+++ src/components/AchievementSummaryTable/AchievementSummaryTable.stories.tsx
@@ -1,52 +1,67 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import React from 'react'
+import React, { useState } from 'react'
 
 import AchievementSummaryTable from '.'
+import { itemType } from '../Dropdowns/Dropdown'
 
 export default {
-  title: 'AchievementSummaryTable',
+  title: 'Achievement/AchievementSummaryTable',
   component: AchievementSummaryTable
 }
 
-export const Default = () => (
-  <AchievementSummaryTable
-    games={[
-      {
-        id: '1',
-        image: 'https://i.imgur.com/Cij5vdL.png',
-        title: 'Diablo II',
-        mintedAchievementsCount: 5,
-        mintableAchievementsCount: 10,
-        totalAchievementsCount: 30
-      },
-      {
-        id: '2',
-        image: 'https://i.imgur.com/Cij5vdL.png',
-        title: 'Star Wars: Knights of the Old Republic',
-        mintedAchievementsCount: 5,
-        mintableAchievementsCount: 10,
-        totalAchievementsCount: 30,
-        ctaProps: { disabled: true }
-      },
-      {
-        id: '3',
-        image: 'https://i.imgur.com/Cij5vdL.png',
-        title: 'Star Wars: Knights of the Old Republic',
-        mintedAchievementsCount: 5,
-        mintableAchievementsCount: 10,
-        totalAchievementsCount: 30,
-        state: 'active'
-      },
-      {
-        id: '3',
-        image: '',
-        title: 'Star Wars: Knights of the Old Republic',
-        mintedAchievementsCount: 5,
-        mintableAchievementsCount: 10,
-        totalAchievementsCount: 30,
-        state: 'update',
-        isNewAchievement: true
-      }
-    ]}
-  />
-)
+export const Default = () => {
+  const achievementsSortOptions = [{ text: 'Alphabetically' }] as itemType[]
+  const [selectedSort, setSelectedSort] = useState(achievementsSortOptions[0])
+  return (
+    <AchievementSummaryTable
+      games={[
+        {
+          id: '1',
+          image: 'https://i.imgur.com/Cij5vdL.png',
+          title: 'Diablo II',
+          mintedAchievementsCount: 5,
+          mintableAchievementsCount: 10,
+          totalAchievementsCount: 30
+        },
+        {
+          id: '2',
+          image: 'https://i.imgur.com/Cij5vdL.png',
+          title: 'Star Wars: Knights of the Old Republic',
+          mintedAchievementsCount: 5,
+          mintableAchievementsCount: 10,
+          totalAchievementsCount: 30,
+          ctaProps: { disabled: true }
+        },
+        {
+          id: '3',
+          image: 'https://i.imgur.com/Cij5vdL.png',
+          title: 'Star Wars: Knights of the Old Republic',
+          mintedAchievementsCount: 5,
+          mintableAchievementsCount: 10,
+          totalAchievementsCount: 30,
+          state: 'active'
+        },
+        {
+          id: '3',
+          image: '',
+          title: 'Star Wars: Knights of the Old Republic',
+          mintedAchievementsCount: 5,
+          mintableAchievementsCount: 10,
+          totalAchievementsCount: 30,
+          isNewAchievement: true
+        }
+      ]}
+      sortProps={{
+        options: achievementsSortOptions,
+        selected: selectedSort,
+        onItemChange: setSelectedSort
+      }}
+      paginationProps={{
+        currentPage: 1,
+        totalPages: 3,
+        handleNextPage: () => console.log('next page'),
+        handlePrevPage: () => console.log('prev page')
+      }}
+    />
+  )
+}
--- src/components/AchievementSummaryTable/index.tsx
+++ src/components/AchievementSummaryTable/index.tsx
@@ -1,10 +1,10 @@
-import React, { HTMLProps, useState } from 'react'
+import React, { HTMLProps } from 'react'
 
 import AchievementCard, { AchievementCardProps } from '../AchievementCard'
 // import AchievementPageNav from '../AchievementPageNav'
 import Button from '../Button'
 import { Dropdown } from '../Dropdowns'
-import { itemType } from '../Dropdowns/Dropdown'
+import { DropdownProps } from '../Dropdowns/Dropdown'
 import Tabs from '../Tabs'
 import styles from './AchievementSummaryTable.module.scss'
 
@@ -13,54 +13,56 @@ interface Game extends AchievementCardProps {
 }
 
 export interface GameAchievementsProps extends HTMLProps<HTMLDivElement> {
-  /**
-   * list of options for the dropdown to sort your achievements
-   */
-  sortOptions?: itemType[]
   allFilterLabel?: string
   newFilterLabel?: string
   mintedFilterLabel?: string
   mintButtonLabel?: string
   games: Game[]
+  sortProps: DropdownProps
+  paginationProps: {
+    currentPage: number
+    totalPages: number
+    handleNextPage: () => void
+    handlePrevPage: () => void
+  }
 }
 
 export default function AchievementSummaryTable({
-  sortOptions = [
-    { text: 'Alphabetically' },
-    { text: 'Favorites' },
-    { text: 'Sort by Status' }
-  ],
   allFilterLabel = 'All',
   newFilterLabel = 'New',
   mintedFilterLabel = 'Minted',
   mintButtonLabel = 'Mint',
   games,
+  sortProps,
+  paginationProps,
   ...rest
 }: GameAchievementsProps) {
-  const [selected, setSelected] = useState(sortOptions[0])
-
   return (
     <div className={styles.container} {...rest}>
       {/* <AchievementPageNav
         freeMints={10}
         basketAmount={0}
-        handleNext={() => console.log('next')}
-        handlePrevious={() => console.log('previous')}
+        nextButtonProps={{
+          onClick: handleNextPage,
+          disabled: currentPage === totalPages
+        }}
+        previousButtonProps={{
+          onClick: handlePrevPage,
+          disabled: currentPage === 1
+        }}
       /> */}
       <Tabs defaultValue="all">
         <div className={styles.row}>
           <div className={styles.filters}>
             <Dropdown
-              options={sortOptions}
-              onItemChange={setSelected}
-              selected={selected}
               targetWidth="300"
               dropdownButtonDivProps={{
                 className: 'title-sm'
               }}
+              {...sortProps}
             />
 
-            <Tabs.List type="outline">
+            <Tabs.List type="outline" style={{ height: '50px' }}>
               <Tabs.Tab value="all">
                 <div className="menu">{allFilterLabel}</div>
               </Tabs.Tab>
