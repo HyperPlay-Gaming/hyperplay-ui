15cd43357207a85788663709fbfe947af862c6a5
Elio Briceño <eliobricenov@gmail.com>
2023-09-18 21:05:52 -0400

    fix: fix storybook build

--- src/components/CheckEmail/CheckEmail.stories.tsx
+++ src/components/CheckEmail/CheckEmail.stories.tsx
@@ -19,6 +19,7 @@ export const Default: Story = {
       style={{ margin: 'auto' }}
       email="hello@hyperplay.xyz"
       onResend={() => alert('Resend email')}
+      onVerify={() => alert('Resend email')}
     />
   )
 }
