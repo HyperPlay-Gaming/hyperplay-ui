822896d37b82472fca52f737c1f9f20d4b3e17f0
Flavio F Lima <flavioislima@users.noreply.github.com>
2023-09-21 13:26:50 +0100

    feat: make GameCard stories dynamic

--- src/components/GameCard/GameCard.stories.tsx
+++ src/components/GameCard/GameCard.stories.tsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 import MoonBlastersCover from '@/assets/MoonBlastersCover.png?url'
 import RocketMonstersCover from '@/assets/RocketMonstersCover.png?url'
 
-import GameCard from '.'
-import { Runner } from './types'
+import GameCard, { GameCardProps } from '.'
 
-export default {
-  title: 'GameCard'
-}
-
-const onClickHandlers = {
+const props: GameCardProps = {
+  store: 'hyperplay',
+  title: 'Test Game',
+  state: 'NOT_INSTALLED',
   onDownloadClick: () => console.log('download clicked'),
   onFavoriteClick: () => console.log('favorite clicked'),
   onPlayClick: () => console.log('play clicked'),
@@ -36,211 +34,52 @@ const onClickHandlers = {
   onSettingsClick: () => console.log('settings button clicked'),
   onUpdateClick: () => console.log('update button clicked'),
   onResumeClick: () => console.log('resume button clicked'),
-  store: 'hyperplay' as Runner
 }
 
-export const DefaultUninstalled = () => {
-  const [favorited, setFavorited] = useState(false)
-  return (
-    <GameCard
-      title="Test Game"
-      {...onClickHandlers}
-      state="NOT_INSTALLED"
-      favorited={favorited}
-      onFavoriteClick={() => setFavorited(!favorited)}
-    ></GameCard>
-  )
+const meta: Meta<typeof GameCard> = {
+  title: 'GameCard',
+  component: GameCard,
+  parameters: {
+    store: {
+      values: [
+        {name: 'HyperPlay', value : 'hyperplay'},
+        {name: 'GOG', value : 'gog'},
+        {name: 'Epic', value : 'legendary'},
+        {name: 'Amazon', value : 'nile'},
+        {name: 'Sideload', value : 'sideload'}
+      ],
+    },
+    state: {
+      values: [
+        'NOT_INSTALLED',
+        'QUEUED',
+        'PLAYING',
+        'INSTALLED',
+        'SHOW_MESSAGE',
+        'NOT_SUPPORTED',
+        'UNINSTALLING',
+        'INSTALLING',
+        'PAUSED',
+        'NEEDS_UPDATE',
+      ],
+    }
+  },
+};
+
+
+
+type Story = StoryObj<typeof GameCard>
+
+export const HyperPlayCard: Story = {
+  args: {...props}
 }
 
-export const LongTitleUninstalled = () => (
-  <GameCard
-    title="Test Game with a Really Long Title"
-    {...onClickHandlers}
-    state="NOT_INSTALLED"
-  ></GameCard>
-)
-
-export const Queued = () => (
-  <GameCard title="Test Game" {...onClickHandlers} state="QUEUED"></GameCard>
-)
-
-export const Playing = () => (
-  <GameCard title="Test Game" {...onClickHandlers} state="PLAYING"></GameCard>
-)
-
-export const Installed = () => {
-  /* eslint-disable-next-line */
-  const { onFavoriteClick, ...handlers } = onClickHandlers
-  const [favorited, setFavorited] = useState(false)
-  return (
-    <GameCard
-      title="Test Game"
-      {...handlers}
-      onFavoriteClick={() => setFavorited(!favorited)}
-      state="INSTALLED"
-      favorited={favorited}
-      imageUrl={MoonBlastersCover}
-    ></GameCard>
-  )
+export const MoonBlasters: Story = {
+  args: {...props, imageUrl: MoonBlastersCover, title: 'Moon Blasters'}
 }
 
-export const ShowMessage = () => (
-  <GameCard
-    title="Test Game"
-    {...onClickHandlers}
-    state="SHOW_MESSAGE"
-    message="Test message..."
-    imageUrl={RocketMonstersCover}
-  ></GameCard>
-)
-
-export const NotSupported = () => (
-  <GameCard
-    title="Test Game"
-    {...onClickHandlers}
-    state="NOT_SUPPORTED"
-    message="Test message..."
-  ></GameCard>
-)
-
-export const Uninstalling = () => (
-  <GameCard
-    title="Test Game"
-    {...onClickHandlers}
-    state="UNINSTALLING"
-    message="Test message..."
-  ></GameCard>
-)
-
-export const Installing = () => {
-  const [showSettings, setShowSettings] = useState(true)
-  return (
-    <GameCard
-      title="Test Game"
-      {...onClickHandlers}
-      state="INSTALLING"
-      progress={{ bytes: '0', percent: 50 }}
-      message="Installing..."
-      onContextMenu={(e) => {
-        e.preventDefault()
-        setShowSettings(!showSettings)
-      }}
-      showSettings={showSettings}
-      onSettingsClick={() => setShowSettings(!showSettings)}
-    ></GameCard>
-  )
+export const RocketMonsters: Story = {
+  args: {...props, imageUrl: RocketMonstersCover, title: 'Rocket Monsters'}
 }
 
-export const Paused = () => (
-  <GameCard
-    title="Test Game"
-    {...onClickHandlers}
-    state="PAUSED"
-    progress={{ bytes: '0', percent: 50 }}
-    message="Paused"
-  ></GameCard>
-)
-
-export const TwoInstalled = () => {
-  return (
-    <div style={{ display: 'flex' }}>
-      <GameCard
-        title="Test Game"
-        {...onClickHandlers}
-        state="INSTALLED"
-        imageUrl={MoonBlastersCover}
-      ></GameCard>
-      <GameCard
-        title="Test Game"
-        {...onClickHandlers}
-        state="INSTALLED"
-        imageUrl={MoonBlastersCover}
-      ></GameCard>
-    </div>
-  )
-}
-
-export const InstalledContextMenu = () => {
-  const [showSettings, setShowSettings] = useState(true)
-  return (
-    <GameCard
-      title="Test Game"
-      {...onClickHandlers}
-      state="INSTALLED"
-      imageUrl={MoonBlastersCover}
-      onContextMenu={(e) => {
-        e.preventDefault()
-        setShowSettings(!showSettings)
-      }}
-      showSettings={showSettings}
-      onSettingsClick={() => setShowSettings(!showSettings)}
-    ></GameCard>
-  )
-}
-
-export const NeedsUpdate = () => {
-  const [showSettings, setShowSettings] = useState(false)
-  return (
-    <GameCard
-      title="Test Game"
-      {...onClickHandlers}
-      state="NEEDS_UPDATE"
-      imageUrl={MoonBlastersCover}
-      onContextMenu={(e) => {
-        e.preventDefault()
-        setShowSettings(!showSettings)
-      }}
-      showSettings={showSettings}
-      onSettingsClick={() => setShowSettings(!showSettings)}
-    ></GameCard>
-  )
-}
-
-export const Store = () => {
-  const [inLibrary, setInLibrary] = useState(false)
-  return (
-    <GameCard
-      title="Test Game"
-      {...onClickHandlers}
-      state="NEEDS_UPDATE"
-      app="store"
-      onAddToLibraryClick={() => setInLibrary(true)}
-      onRemoveFromLibraryClick={() => setInLibrary(false)}
-      gameIsAddedToLibrary={inLibrary}
-    ></GameCard>
-  )
-}
-
-export const StoreInClient = () => {
-  const [inLibrary, setInLibrary] = useState(false)
-  return (
-    <GameCard
-      title="Test Game"
-      {...onClickHandlers}
-      state="NEEDS_UPDATE"
-      app="storeInClient"
-      onAddToLibraryClick={() => setInLibrary(true)}
-      onRemoveFromLibraryClick={() => setInLibrary(false)}
-      gameIsAddedToLibrary={inLibrary}
-      enableRemoveButton={true}
-    ></GameCard>
-  )
-}
-
-export const StoreInClientRemoveDisabled = () => {
-  const [inLibrary, setInLibrary] = useState(false)
-  return (
-    <GameCard
-      title="Test Game"
-      {...onClickHandlers}
-      state="NEEDS_UPDATE"
-      app="storeInClient"
-      onAddToLibraryClick={() => setInLibrary(true)}
-      onRemoveFromLibraryClick={() => setInLibrary(false)}
-      gameIsAddedToLibrary={inLibrary}
-      enableRemoveButton={false}
-      notAddedText={'Add'}
-      addedText="In library"
-    ></GameCard>
-  )
-}
+export default meta
