edcba7147ea0bd1da62cf3382fa841a9537678e9
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2023-10-26 18:03:37 -0400

    Feat/auth components use client (#100)
    * Revert "refactor: add use client directive to auth components and modal"
    
    This reverts commit 8045eeade1b52814e3cc574f38a31700e5ff76da.
    
    * refactor: bump version

--- src/components/EmailVerified/index.tsx
+++ src/components/EmailVerified/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { HTMLProps } from 'react'
 
 import cn from 'classnames'
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { HTMLProps, useState } from 'react'
 
 import cn from 'classnames'
