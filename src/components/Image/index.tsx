6ecba92ff5446f1b9d3bf8fd9ea0468ff71a7169
Red <red.pace.dev@gmail.com>
2023-11-23 00:50:17 +0100

    feat: Added custom image from mantine, since we're passing components

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState, ReactNode, forwardRef } from 'react';
+import { Image, ImageProps, createPolymorphicComponent } from '@mantine/core';
+
+export interface CustomImageProps extends ImageProps {
+  placeholder?: string | ReactNode;
+}
+
+const CustomImage = createPolymorphicComponent<'img', CustomImageProps>(
+  forwardRef<HTMLImageElement, CustomImageProps>(function CustomImage(
+    { placeholder: Placeholder, fallbackSrc, ...props }: CustomImageProps, ref) {
+  const [loadError, setLoadError] = useState(false);
+
+  console.log(props)
+  
+  if (loadError && Placeholder || loadError && fallbackSrc) {
+    if (typeof Placeholder === 'string' || fallbackSrc) {
+      return (
+        <Image 
+          ref={ref}
+          fallbackSrc={fallbackSrc || Placeholder as string}
+          onError={(error) => {
+            setLoadError(true)
+            
+            props?.onError?.(error);
+          }}
+          {...props}
+        />
+      )
+    } 
+
+    return <>{Placeholder}</>;
+  }
+
+  return (
+    <Image
+      ref={ref}
+      onError={(error) => {
+        setLoadError(true)
+        
+        props?.onError?.(error);
+      }}
+      {...props}
+    />
+  );
+}));
+
+export default CustomImage;
