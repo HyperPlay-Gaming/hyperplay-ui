083e33cbf12cd1f8e2b75d75e957c623e1c82ccc
Brett <27568879+BrettCleary@users.noreply.github.com>
2023-12-11 17:06:08 -0800

    add zksync banner

---
+++ src/components/ZkSyncQuestBanner/index.tsx
@@ -0,0 +1,44 @@
+import React, { HTMLProps } from 'react'
+
+import Button from '../Button'
+import styles from './index.module.scss'
+import CapsuleUrl from '@/assets/banners/ZkSync/Capsule.png?url'
+import classNames from 'classnames'
+
+export interface ZkSyncQuestBannerProps extends HTMLProps<HTMLDivElement> {
+  i18n?: {
+    title?: string
+    description?: JSX.Element
+    buttonText?: string
+  }
+  link: string
+}
+
+export default function ZkSyncQuestBanner({
+  i18n,
+  link,
+  ...props
+}: ZkSyncQuestBannerProps) {
+  const { title, description, buttonText } = i18n || {
+    title: 'NEW QUEST AVAILABLE',
+    description: (
+      <div className={classNames('title', styles.title)}>
+        Existing capsule holders, complete the quest to
+        <b> earn special zkSync on-chain rewards!</b>
+      </div>
+    ),
+    buttonText: 'View Quest'
+  }
+  return (
+    <div className={styles.bannerContainer} {...props}>
+        <div className={styles.contentContainer}>
+            <h3>{title}</h3>
+            {description}
+            <div className={styles.cta}>
+            <a href={link} rel="noreferrer" target="_blank"><Button type='secondary'><div className={styles.buttonTextContainer}>{buttonText}</div></Button></a>
+            </div>
+        </div>
+        <img src={CapsuleUrl} className={styles.capsule}/>
+    </div>
+  )
+}
