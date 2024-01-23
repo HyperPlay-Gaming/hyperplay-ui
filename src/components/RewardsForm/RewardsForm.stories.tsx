c9b3b571cde8a2e968e063887b6fade93152ab79
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2024-01-22 21:52:41 -0800

    add rewards form

---
+++ src/components/RewardsForm/RewardsForm.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import RewardsForm from './index'
+
+export default {
+  title: 'Quests/RewardsForm',
+  component: RewardsForm
+}
+
+export const Default = () => <RewardsForm />
