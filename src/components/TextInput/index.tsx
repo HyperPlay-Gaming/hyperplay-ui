9f43a2c951fc4e2d0032b31587a138ba2c40ef25
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2024-01-22 21:31:20 -0800

    add character counter

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 
 import { TextInput as MantineTextInput } from '@mantine/core'
 import cn from 'classnames'
 
 import styles from './TextInput.module.scss'
 
-interface TextInputProps
+export interface TextInputProps
   extends React.ComponentPropsWithoutRef<typeof MantineTextInput> {
   classNames?: {
     root?: string
@@ -13,12 +13,22 @@ interface TextInputProps
     wrapper?: string
     label?: string
   }
+  maxCharacters?: number
 }
 
 const TextInput = React.forwardRef<
   React.ElementRef<typeof MantineTextInput>,
   TextInputProps
->(({ classNames, ...props }, ref) => {
+>(({ classNames, rightSection, maxCharacters, onChange, ...props }, ref) => {
+  const [numChars, setNumChars] = useState(0)
+
+  let rightSectionComponent = rightSection
+  if (rightSectionComponent === undefined && maxCharacters !== undefined) {
+    rightSectionComponent = (
+      <div className={styles.charCounter}>{`${numChars}/${maxCharacters}`}</div>
+    )
+  }
+
   return (
     <MantineTextInput
       {...props}
@@ -31,6 +41,18 @@ const TextInput = React.forwardRef<
       }}
       unstyled
       ref={ref}
+      onChange={(ev) => {
+        setNumChars(ev.target.value.length)
+        if (onChange) {
+          onChange(ev)
+        }
+      }}
+      onBeforeInput={(ev) => {
+        if (maxCharacters !== undefined && numChars === maxCharacters) {
+          ev.preventDefault()
+        }
+      }}
+      rightSection={rightSectionComponent}
     />
   )
 })
