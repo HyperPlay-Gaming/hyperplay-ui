80ab5427d07462a03dceb7c2dc7a07efe2242874
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2024-01-23 21:40:24 -0800

    add Address component

--- src/index.ts
+++ src/index.ts
@@ -65,3 +65,4 @@ export {
   type ZkSyncQuestBannerProps
 } from './components/ZkSyncQuestBanner'
 export { default as LinkAccountDialog } from './components/LinkAccount'
+export { default as Address, type AddressProps } from './components/Address'
