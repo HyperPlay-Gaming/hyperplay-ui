56de442f07dbfaf53f6be690fcf751f605a77597
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2024-01-24 00:16:22 -0800

    add card

--- src/index.ts
+++ src/index.ts
@@ -65,3 +65,4 @@ export {
   type ZkSyncQuestBannerProps
 } from './components/ZkSyncQuestBanner'
 export { default as LinkAccountDialog } from './components/LinkAccount'
+export { default as Card, type CardProps } from './components/Card'
