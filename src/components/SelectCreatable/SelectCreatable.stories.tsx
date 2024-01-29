b8b5f1609900758ed2e8c9b3b07131ddef4c69c5
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-01-29 11:45:23 -0800

    [Valist Migration] Add CreatableSelect (#154)
    * init
    
    * rename to SelectCreatable, update props
    
    * fix styling
    
    * export component
    
    * add i18n
    
    * bump 1.1.0

---
+++ src/components/SelectCreatable/SelectCreatable.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SelectCreatable, SelectCreatableProps } from '.'
+
+const meta: Meta<typeof SelectCreatable> = {
+  title: 'DevPortal/SelectCreatable',
+  component: SelectCreatable
+}
+
+export default meta
+
+type Story = StoryObj<typeof SelectCreatable>
+
+const props: SelectCreatableProps = {
+  options: [
+    '🍎 Apples',
+    '🍌 Bananas',
+    '🥦 Broccoli',
+    '🥕 Carrots',
+    '🍫 Chocolate',
+    '🍇 Grapes'
+  ],
+  onChange: (val) => console.log('changed to ', val),
+  onCreated: (val) => console.log('created ', val)
+}
+
+export const Default: Story = {
+  args: { ...props }
+}
---
+++ src/components/SelectCreatable/index.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+
+import { Combobox, ComboboxProps, InputBase, useCombobox } from '@mantine/core'
+
+import styles from './SelectCreatable.module.scss'
+
+export interface SelectCreatableProps extends ComboboxProps {
+  options: string[]
+  onChange: (option: string) => void
+  onCreated: (option: string) => void
+  i18n?: {
+    create?: string
+    searchValue?: string
+  }
+}
+
+export function SelectCreatable({
+  options,
+  onChange,
+  onCreated,
+  i18n = { create: '+ Create', searchValue: 'Search value' },
+  ...props
+}: SelectCreatableProps) {
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption()
+  })
+
+  const [data, setData] = useState(options)
+  const [value, setValue] = useState<string | null>(null)
+  const [search, setSearch] = useState('')
+
+  const exactOptionMatch = data.some((item) => item === search)
+  const filteredOptions = exactOptionMatch
+    ? data
+    : data.filter((item) =>
+        item.toLowerCase().includes(search.toLowerCase().trim())
+      )
+
+  const optionsElements = filteredOptions.map((item) => (
+    <Combobox.Option value={item} key={item}>
+      {item}
+    </Combobox.Option>
+  ))
+
+  return (
+    <Combobox
+      store={combobox}
+      withinPortal={false}
+      onOptionSubmit={(val) => {
+        if (val === '$create') {
+          setData((current) => [...current, search])
+          setValue(search)
+          onCreated(search)
+        } else {
+          setValue(val)
+          setSearch(val)
+          onChange(val)
+        }
+
+        combobox.closeDropdown()
+      }}
+      classNames={{ dropdown: styles.dropdown, option: styles.option }}
+      {...props}
+    >
+      <Combobox.Target>
+        <InputBase
+          rightSection={<Combobox.Chevron />}
+          value={search}
+          onChange={(event) => {
+            combobox.openDropdown()
+            combobox.updateSelectedOptionIndex()
+            setSearch(event.currentTarget.value)
+          }}
+          onClick={() => combobox.openDropdown()}
+          onFocus={() => combobox.openDropdown()}
+          onBlur={() => {
+            combobox.closeDropdown()
+            setSearch(value || '')
+          }}
+          placeholder={i18n.searchValue}
+          rightSectionPointerEvents="none"
+          classNames={{ input: styles.input }}
+        />
+      </Combobox.Target>
+
+      <Combobox.Dropdown>
+        <Combobox.Options>
+          {optionsElements}
+          {!exactOptionMatch && search.trim().length > 0 && (
+            <Combobox.Option value="$create">
+              {i18n.create} {search}
+            </Combobox.Option>
+          )}
+        </Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
+  )
+}
