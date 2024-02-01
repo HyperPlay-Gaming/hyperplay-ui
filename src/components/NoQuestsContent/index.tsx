be57a4810ddfe0f635f25a3f156a375208defe28
Red <red.pace.dev@gmail.com>
2024-02-01 20:10:32 +0100

    chore: Replaced mantine components

--- src/components/NoQuestsContent/index.tsx
+++ src/components/NoQuestsContent/index.tsx
@@ -1,12 +1,12 @@
-import { Container, Text, Title } from '@mantine/core'
 import cn from 'classnames'
 
 import { QuestIcon } from '@/assets/images'
 import Button, { ButtonProps } from '@/components/Button'
 
 import styles from './index.module.scss'
+import { InputHTMLAttributes, PropsWithChildren, forwardRef } from 'react'
 
-interface NoQuestsContentProps {
+interface NoQuestsContentProps extends PropsWithChildren<InputHTMLAttributes<HTMLDivElement>> {
   onCreateNewQuest: () => void
   i18n?: i18nNoQuestContentContext
   className?: string
@@ -19,25 +19,29 @@ interface i18nNoQuestContentContext {
   buttonText: string
 }
 
-export default function NoQuestsContent({
-  className: classNameProp,
-  onCreateNewQuest,
-  i18n = {
-    title: 'Create your first Quest',
-    description:
-      'Incentivize players with previous Steam Achievements to try out your game.',
-    buttonText: 'Create New Quest'
-  },
-  buttonProps = {}
-}: NoQuestsContentProps) {
+export default forwardRef<HTMLDivElement, NoQuestsContentProps>(function NoQuestsContent(
+    {
+      className: classNameProp,
+      onCreateNewQuest,
+      i18n = {
+        title: 'Create your first Quest',
+        description:
+          'Incentivize players with previous Steam Achievements to try out your game.',
+        buttonText: 'Create New Quest'
+      },
+      buttonProps = {},
+      ...props
+    }: NoQuestsContentProps,
+    ref
+  ) {
   return (
-    <Container className={cn(styles.container, classNameProp)}>
-      <QuestIcon width={120} height={120} />
-      <Title className={styles.title}>{i18n.title}</Title>
-      <Text className={styles.description}>{i18n.description}</Text>
+    <div className={cn(styles.container, classNameProp)} ref={ref} {...props} >
+      <QuestIcon className={styles.questIcon} />
+      <h1 className={styles.title}>{i18n.title}</h1>
+      <p className={styles.description}>{i18n.description}</p>
       <Button onClick={onCreateNewQuest} {...buttonProps}>
         {i18n.buttonText}
       </Button>
-    </Container>
+    </div>
   )
-}
+});
