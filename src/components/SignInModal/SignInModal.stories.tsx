8415c48ebefefeeeb26750a046527a63b3d6753a
Elio Briceño <eliobricenov@gmail.com>
2024-02-02 19:07:30 -0400

    refactor(auth): use only email input

---
+++ src/components/SignInModal/SignInModal.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import { Meta, StoryObj } from '@storybook/react'
+
+import SignInModal from '@/components/SignInModal/index'
+
+const meta: Meta<typeof SignInModal> = {
+  title: 'auth/SignInModal'
+}
+
+export default meta
+
+type Story = StoryObj<typeof SignInModal>
+
+export const Default: Story = {
+  render: () => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <SignInModal
+        onClose={() => alert('Close')}
+        onSubmit={(email) => alert(`Email requested: ${email}`)}
+      />
+    </div>
+  )
+}
+
+export const Loading: Story = {
+  render: () => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <SignInModal
+        loading={true}
+        onClose={() => alert('Close')}
+        onSubmit={(email) => alert(`Email requested: ${email}`)}
+      />
+    </div>
+  )
+}
+
+export const Error: Story = {
+  render: () => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <SignInModal
+        error="Woops! Explain error."
+        onClose={() => alert('Close')}
+        onSubmit={(email) => alert(`Email requested: ${email}`)}
+      />
+    </div>
+  )
+}
