42ceab4368c8df57e96dfd54f29dc81d80061c30
Elio Briceño <eliobricenov@gmail.com>
2024-02-02 14:48:51 -0400

    fix(auth): add link account

---
+++ src/components/LinkExternalAccountsModal/LinkExternalAccountsModal.spec.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithMantine } from 'tests/utils/providers'
+import { describe, expect, it, vi } from 'vitest'
+
+import LinkExternalAccountsModal from '@/components/LinkExternalAccountsModal/index'
+
+const providers = [{ id: 'discord', name: 'Discord', icon: 'discord' }]
+
+describe('LinkExternalAccountsModal', () => {
+  it('executes onAuthSignup handler upon selecting a provider', () => {
+    const providerOption = providers[0]
+    const onAuthSignup = vi.fn()
+    renderWithMantine(
+      <LinkExternalAccountsModal
+        onClose={vi.fn}
+        providers={providers}
+        onWalletClick={vi.fn}
+        onAuthProviderClick={onAuthSignup}
+      />
+    )
+
+    // Find and click on the provider button you want to test (e.g., 'Discord')
+    fireEvent.click(screen.getByRole('button', { name: providerOption.name }))
+
+    expect(onAuthSignup).toHaveBeenCalledWith(providerOption)
+  })
+
+  it('executes onWalletSignup handler upon selecting wallet provider', () => {
+    const onWalletClick = vi.fn()
+    renderWithMantine(
+      <LinkExternalAccountsModal
+        onClose={vi.fn}
+        providers={providers}
+        onWalletClick={onWalletClick}
+        onAuthProviderClick={vi.fn}
+      />
+    )
+
+    // Find and click on the provider button you want to test (e.g., 'Discord')
+    fireEvent.click(screen.getByRole('button', { name: /wallet/i }))
+
+    expect(onWalletClick).toHaveBeenCalled()
+  })
+})
---
+++ src/components/LinkExternalAccountsModal/LinkExternalAccountsModal.stories.tsx
@@ -0,0 +1,112 @@
+import React from 'react'
+
+import { Meta, StoryObj } from '@storybook/react'
+
+import {
+  DiscordFilled,
+  EpicStoreLogo,
+  GoogleLogo,
+  KickLogo,
+  SteamLogo,
+  TwitchLogo,
+  XLogo
+} from '@/assets/images'
+
+import LinkExternalAccountsModal from './index'
+
+type Story = StoryObj<typeof LinkExternalAccountsModal>
+
+const meta: Meta<typeof LinkExternalAccountsModal> = {
+  title: 'auth/LinkExternalAccountsModal'
+}
+
+export default meta
+
+const connectedProviders = {
+  metamaskExtension: true,
+  metamaskMobile: false,
+  walletConnect: true
+}
+
+const authProviders = [
+  {
+    id: 'discord',
+    name: 'Discord',
+    icon: <DiscordFilled fill="white" />
+  },
+  {
+    id: 'google',
+    name: 'Google',
+    icon: <GoogleLogo fill="white" />
+  },
+  {
+    id: 'kick',
+    name: 'Kick',
+    icon: <KickLogo fill="white" />
+  },
+  {
+    id: 'steam',
+    name: 'Steam',
+    icon: <SteamLogo fill="white" />
+  },
+  {
+    id: 'twitch',
+    name: 'Twitch',
+    icon: <TwitchLogo fill="white" />
+  },
+  {
+    id: 'twitter',
+    name: 'Twitter/X',
+    icon: <XLogo fill="white" />
+  },
+  {
+    id: 'epic',
+    name: 'Epig Games',
+    icon: <EpicStoreLogo />,
+    disabled: true
+  }
+]
+
+export const Default: Story = {
+  render: () => (
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: 24 }}>
+      <LinkExternalAccountsModal
+        providers={authProviders}
+        onClose={() => alert('Closed')}
+        onWalletClick={() => alert('Wallet linked')}
+        onAuthProviderClick={(provider) => alert(`Provider: ${provider.id}`)}
+      />
+    </div>
+  )
+}
+
+export const WalletProvidersConnected: Story = {
+  render: () => (
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: 24 }}>
+      <LinkExternalAccountsModal
+        providers={authProviders}
+        onClose={() => alert('Closed')}
+        onWalletClick={() => alert('Wallet linked')}
+        onAuthProviderClick={(provider) => alert(`Provider: ${provider.id}`)}
+        walletProvidersConnectionState={connectedProviders}
+      />
+    </div>
+  )
+}
+
+export const Error: Story = {
+  render: () => (
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: 24 }}>
+      <LinkExternalAccountsModal
+        alert={{
+          variant: 'danger',
+          message: 'Something went wrong. Please try again later.'
+        }}
+        providers={authProviders}
+        onClose={close}
+        onWalletClick={() => alert('Wallet linked')}
+        onAuthProviderClick={(provider) => alert(`Provider: ${provider.id}`)}
+      />
+    </div>
+  )
+}
