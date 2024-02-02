de727ada7a983626dcab8e12ecaf77c8c006e547
Elio Briceño <eliobricenov@gmail.com>
2024-02-02 18:57:01 -0400

    styles(alert): add icons and update styles

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,10 +1,22 @@
 import React, { HTMLProps } from 'react'
 
+import {
+  IconAlertCircle,
+  IconAlertTriangle,
+  IconCircleCheck
+} from '@tabler/icons-react'
 import cs from 'classnames'
 
 import styles from './index.module.scss'
 
 type Variants = 'warning' | 'danger' | 'info' | 'success'
+
+const icons: Record<Variants, typeof IconAlertCircle> = {
+  warning: IconAlertTriangle,
+  danger: IconAlertCircle,
+  info: IconCircleCheck,
+  success: IconCircleCheck
+}
 
 export interface AlertProps extends HTMLProps<HTMLDivElement> {
   variant?: Variants
@@ -12,8 +24,10 @@ export interface AlertProps extends HTMLProps<HTMLDivElement> {
 }
 
 const Alert = ({ variant = 'info', message, ...props }: AlertProps) => {
+  const Icon = icons[variant]
   return (
     <div className={cs(styles.base, styles[variant])} {...props}>
+      {Icon && <Icon className={styles.icon} />}
       <span>{message}</span>
     </div>
   )
