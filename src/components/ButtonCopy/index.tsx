e8cffb2d2f5fbec9bbeaa3151f662d56f2e21617
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2024-02-05 14:42:21 -0800

    update button copy

--- src/components/ButtonCopy/index.tsx
+++ src/components/ButtonCopy/index.tsx
@@ -7,29 +7,46 @@ import classNames from 'classnames'
 
 import { Checkmark, Copy } from '@/assets/images'
 
+import Button from '../Button'
 import styles from './ButtonCopy.module.scss'
 
 export interface ButtonCopyProps extends HTMLAttributes<HTMLButtonElement> {
   text: string
+  i18n?: {
+    copy?: string
+    copied?: string
+  }
 }
 
-export function ButtonCopy({ text, className, ...props }: ButtonCopyProps) {
+export function ButtonCopy({
+  text,
+  className,
+  i18n = { copy: 'Copy', copied: 'Copied' },
+  ...props
+}: ButtonCopyProps) {
   const clipboard = useClipboard()
-  const copyButton = (
-    <button
+  let copyButton = <Copy className={styles.copyButton} />
+
+  let copyText = i18n.copy
+
+  if (clipboard.copied) {
+    copyButton = <Checkmark className={styles.copiedCheckmark} />
+    copyText = i18n.copied
+  }
+
+  const rootClassNames: Record<string, boolean> = {}
+  rootClassNames[styles.copied] = clipboard.copied
+
+  return (
+    <Button
+      size="small"
+      type="tertiary"
+      rightIcon={copyButton}
       onClick={() => clipboard.copy(text)}
-      className={classNames('center', className)}
+      className={classNames(styles.root, rootClassNames, className)}
       {...props}
     >
-      <Copy className={styles.copyButton} />
-    </button>
+      {copyText}
+    </Button>
   )
-
-  const copiedElement = (
-    <div className={className}>
-      <Checkmark className={styles.copiedCheckmark} />
-    </div>
-  )
-
-  return clipboard.copied ? copiedElement : copyButton
 }
