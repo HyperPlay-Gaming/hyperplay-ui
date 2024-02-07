41d8887c98e9106b14ee1bfd1c18e9ba74c8b80b
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2024-02-07 17:59:37 -0400

    feat(auth): add account avatar and export spinner (#193)
    * feat(auth): add account avatar and export spinner
    
    * refactor(account-avatar): remove spread from avatar
    
    * chore: bump version
    
    * chore: change email

---
+++ src/components/AccountAvatar/index.tsx
@@ -0,0 +1,35 @@
+import { HTMLAttributes } from 'react'
+
+import { Avatar } from '@mantine/core'
+import cn from 'classnames'
+import makeBlockie from 'ethereum-blockies-base64'
+
+import styles from './styles.module.scss'
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  userId?: string
+}
+
+export default function AccountAvatar({ userId, className, ...others }: Props) {
+  const offline = !userId
+
+  if (offline) {
+    return (
+      <div className={cn(styles.container, className)} {...others}>
+        <div className={cn(styles.avatar, styles.offlineAvatar)}>?</div>
+        <span className={cn(styles.status, styles.offline)} />
+      </div>
+    )
+  }
+
+  return (
+    <div className={cn(styles.container, className)} {...others}>
+      <Avatar
+        src={makeBlockie(userId)}
+        size={10}
+        className={cn(styles.avatar, styles.onlineAvatar)}
+      />
+      <span className={cn(styles.status, styles.online)} />
+    </div>
+  )
+}
