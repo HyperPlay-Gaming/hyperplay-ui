90ee022957ce850e75a8e238f43819fc7169d668
Red <red.pace.dev@gmail.com>
2024-02-09 01:49:14 +0100

    feat: Added sticker for pending deposits

--- src/components/RewardDeposit/index.tsx
+++ src/components/RewardDeposit/index.tsx
@@ -5,6 +5,7 @@ import { ContainerInteractive } from '../ContainerInteractive'
 import { RewardDetails, RewardDetailsProps } from '../RewardDetails'
 import styles from './RewardDeposit.module.scss'
 import { FormRewards, FormRewardsProps } from './components/FormRewards'
+import Sticker from '@/components/Sticker'
 
 export interface RewardDepositProps {
   title: string
@@ -17,6 +18,12 @@ export interface RewardDepositProps {
   updateEditable: (editable: boolean) => void
   i18n?: {
     confirm?: string
+    pendingDeposit?: string
+    tokenIdsTitle: string
+    orAddManually: string
+    callToActionAddToken: string
+    addedTokenCounterText: string
+    collapseAllIds: string
   }
   rewardDetailsProps: RewardDetailsProps
 }
@@ -27,12 +34,25 @@ export function RewardDeposit({
   rewardsProps,
   editable,
   updateEditable,
-  i18n = { confirm: 'Confirm Changes' },
+  i18n = { 
+    confirm: 'Confirm Changes',
+    pendingDeposit: 'Pending Deposit',
+    tokenIdsTitle: 'Token IDs',
+    orAddManually: 'Or add manually',
+    callToActionAddToken: 'Add Token IDs',
+    addedTokenCounterText: 'IDs added:',
+    collapseAllIds: 'Collapse all IDs'
+  },
   rewardDetailsProps
 }: RewardDepositProps) {
+  const tag: ReactElement = (
+    <Sticker styleType="warning" variant="filled" className={styles.pendingDepositLabel}>
+      {i18n.pendingDeposit}
+    </Sticker>
+  );
   let content = <RewardDetails {...rewardDetailsProps} />
   if (editable) {
-    content = <FormRewards {...rewardsProps} />
+    content = <FormRewards {...rewardsProps} i18n={i18n} />
   }
 
   let confirmButton = null
@@ -48,6 +68,7 @@ export function RewardDeposit({
     <ContainerInteractive
       title={title}
       icon={icon}
+      tag={tag}
       classNames={{ root: styles.root }}
     >
       {content}
