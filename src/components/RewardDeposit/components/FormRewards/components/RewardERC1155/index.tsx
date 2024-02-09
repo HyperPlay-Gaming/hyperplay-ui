216d04d76016de17447264d6b6ddcc8c73e06c98
Red <red.pace.dev@gmail.com>
2024-02-09 01:53:02 +0100

    feat: Added ERC1155 form

--- src/components/RewardDeposit/components/FormRewards/components/RewardERC1155/index.tsx
+++ src/components/RewardDeposit/components/FormRewards/components/RewardERC1155/index.tsx
@@ -2,56 +2,46 @@
 
 import React from 'react'
 
-import { IconPlus } from '@tabler/icons-react'
-
-import Button from '@/components/Button'
 import TextInput, { TextInputProps } from '@/components/TextInput'
 
-import { TokenIdRowInputProps } from '../../types'
-import { TokenIdRow } from './components/TokenIdRow'
 import styles from './index.module.scss'
 
 export interface RewardERC1155Props {
-  marketplaceUrlTextInputProps: TextInputProps
-  erc1155RewardInputs: TokenIdRowInputProps[]
-  addTokenId: () => void
-  i18n: {
-    addTokenId: string
-  }
+  tokenIdsNumberInputs: TextInputProps[]
+  totalPlayerReachNumberInputs: TextInputProps[]
+  i18n?: {
+    tokenIdsTitle: string
+  },
 }
 
 export function RewardERC1155({
-  marketplaceUrlTextInputProps,
-  erc1155RewardInputs,
-  addTokenId,
-  i18n
+  tokenIdsNumberInputs,
+  totalPlayerReachNumberInputs,
+  i18n = {
+    tokenIdsTitle: 'Token IDs'
+  },
 }: RewardERC1155Props) {
-  const tokenIdRows = erc1155RewardInputs.map((inputs_i, index) => (
-    <TokenIdRow
+  const tokenIdsNumberInput = tokenIdsNumberInputs.map((tokenIdInputProps, index) => (
+    <TextInput
       key={index}
-      {...inputs_i}
-      onRemoveClick={inputs_i.onRemoveClick}
+      {...tokenIdInputProps}
     />
   ))
+  const totalPlayerReachNumberInput = totalPlayerReachNumberInputs.map((tokenIdInputProps, index) => (
+    <TextInput
+      key={index}
+      className={styles.totalPlayerReachNumberInput}
+      {...tokenIdInputProps}
+    />
+  ))
+
   return (
-    <>
-      <div>
-        <div className={styles.tokenIdContainer}>
-          {tokenIdRows}
-          <Button
-            onClick={addTokenId}
-            type="link"
-            leftIcon={<IconPlus />}
-            className={styles.addTokenIdButton}
-          >
-            {i18n.addTokenId}
-          </Button>
-        </div>
-        <TextInput
-          className={styles.marketplaceUrlInput}
-          {...marketplaceUrlTextInputProps}
-        />
+    <div>
+      <div className={styles.tokenIdContainer}>
+        <h6 className={styles.title}>{i18n.tokenIdsTitle}</h6>
+        {tokenIdsNumberInput}
       </div>
-    </>
+      {totalPlayerReachNumberInput}
+    </div>
   )
 }
