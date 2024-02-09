cd6d5822a64586b18d94a9e95528f63d0628476d
Red <red.pace.dev@gmail.com>
2024-02-09 05:54:39 +0100

    fix: Fixed getTruncatedAddress may break on slice if not provided

--- src/utils/addressUtils.ts
+++ src/utils/addressUtils.ts
@@ -1,7 +1,5 @@
-// need to keep type as undefined because hyperplay-dev create quests form sometimes passes undefined
-export function getTruncatedAddress(addr?: string) {
-  if (addr === undefined) {
-    return ''
-  }
+export function getTruncatedAddress(addr: string) {
+  if (!addr) return ''
+  
   return addr.slice(0, 5) + '...' + addr.slice(-5)
 }
