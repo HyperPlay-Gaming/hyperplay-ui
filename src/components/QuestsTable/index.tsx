1f5456227ff87e27f7a9d84e885a342442fb18a0
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-02-10 00:54:50 -0800

    [Quests] Add Quests Table (#198)
    * init
    
    * style rows and improve button UX
    
    * update quest status cell styling
    
    * add on click
    
    * fix action row margin bottom
    
    * add claims
    
    * add +X more to reward strings
    
    * bump 1.1.30

---
+++ src/components/QuestsTable/index.tsx
@@ -0,0 +1,193 @@
+'use client'
+
+import React, { useState } from 'react'
+
+import cn from 'classnames'
+
+import Button from '../Button'
+import styles from './QuestsTable.module.scss'
+
+export interface RewardSimple {
+  amountPerPlayer: number
+  symbol: string
+  balance: number
+}
+
+type statusType = 'DRAFT' | 'ACTIVE'
+
+interface QuestsTableI18n {
+  name?: string
+  games?: string
+  rewardPerPlayer?: string
+  balance?: string
+  claims?: string
+  status?: string
+  player?: string
+  active?: string
+  inactive?: string
+  draft?: string
+}
+
+function getStatusDisplayName(status: statusType, i18n: QuestsTableI18n) {
+  switch (status) {
+    case 'DRAFT':
+      return i18n.draft
+      break
+    case 'ACTIVE':
+      return i18n.active
+      break
+    default:
+      return '???'
+      break
+  }
+}
+
+export interface Quest {
+  name: string
+  rewards: RewardSimple[]
+  numGames: number
+  status: statusType
+  onClick: () => void
+  claims: number
+}
+
+export interface QuestsTableProps {
+  quests: Quest[]
+  i18n?: QuestsTableI18n
+}
+
+export function QuestsTable({
+  quests,
+  i18n = {
+    name: 'Name',
+    games: 'Games',
+    rewardPerPlayer: 'Reward / Player',
+    balance: 'Balance',
+    claims: 'Claims',
+    status: 'Status',
+    player: 'Player',
+    active: 'Active',
+    inactive: 'Inactive',
+    draft: 'Draft'
+  }
+}: QuestsTableProps) {
+  const [filter, setFilter] = useState<null | 'ACTIVE' | 'INACTIVE'>(null)
+
+  let filteredQuests = quests
+  const isActive = filter === 'ACTIVE'
+  const isInactive = filter === 'INACTIVE'
+  const noFilterSelected = filter === null
+  if (isActive) {
+    filteredQuests = filteredQuests.filter((val) => val.status === 'ACTIVE')
+  } else if (isInactive) {
+    filteredQuests = filteredQuests.filter((val) => val.status !== 'ACTIVE')
+  }
+
+  function getStringFromArray(
+    rewards: RewardSimple[],
+    getStringFromReward: (reward: RewardSimple) => string
+  ) {
+    let rewardString = ''
+    for (let i = 0; i < 2 && i < rewards.length; ++i) {
+      const val = rewards[i]
+      if (i === 1) {
+        rewardString += ', '
+      }
+      rewardString += getStringFromReward(val)
+    }
+    const numOfRewardsLeft = rewards.length - 2
+    if (numOfRewardsLeft > 0) {
+      rewardString += `, +${numOfRewardsLeft} more`
+    }
+
+    return rewardString
+  }
+
+  function getRewardString(quest: Quest) {
+    return getStringFromArray(
+      quest.rewards,
+      (reward: RewardSimple) => `${reward.amountPerPlayer}${reward.symbol}`
+    )
+  }
+
+  function getBalanceString(quest: Quest) {
+    return getStringFromArray(
+      quest.rewards,
+      (reward: RewardSimple) => `${reward.balance} ${reward.symbol}`
+    )
+  }
+
+  return (
+    <div>
+      <div className={styles.actionButtonRow}>
+        <Button
+          type={isActive ? 'secondary' : 'tertiary'}
+          onClick={() => {
+            if (isInactive || noFilterSelected) {
+              setFilter('ACTIVE')
+            } else if (isActive) {
+              setFilter(null)
+            }
+          }}
+        >
+          {i18n.active}
+        </Button>
+        <Button
+          type={isInactive ? 'secondary' : 'tertiary'}
+          onClick={() => {
+            if (isActive || noFilterSelected) {
+              setFilter('INACTIVE')
+            } else if (isInactive) {
+              setFilter(null)
+            }
+          }}
+        >
+          {i18n.inactive}
+        </Button>
+      </div>
+      <table className={styles.questsTableContainer}>
+        {filteredQuests.map((quest) => (
+          <tr
+            key={quest.name}
+            className={styles.rowContainer}
+            onClick={quest.onClick}
+          >
+            <td>
+              <div>{i18n.name}</div>
+              <div>{quest.name}</div>
+            </td>
+            <td>
+              <div>{i18n.games}</div>
+              <div>{quest.numGames}</div>
+            </td>
+            <td>
+              <div>{i18n.rewardPerPlayer}</div>
+              <div>{getRewardString(quest)}</div>
+            </td>
+            <td>
+              <div>{i18n.balance}</div>
+              <div>{getBalanceString(quest)}</div>
+            </td>
+            <td>
+              <div>{i18n.status}</div>
+              <div className={styles.statusContainer}>
+                <div
+                  className={cn(
+                    'circle',
+                    styles.statusCircle,
+                    styles[quest.status]
+                  )}
+                ></div>
+                {getStatusDisplayName(quest.status, i18n)}
+              </div>
+            </td>
+            <td>
+              <div>{i18n.claims}</div>
+              <div>{quest.claims}</div>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
+}
