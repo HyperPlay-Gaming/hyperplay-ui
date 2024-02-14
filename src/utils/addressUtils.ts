439375c51bc1f6f20fe850644f1ad9b87110475b
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2024-02-13 22:47:47 -0800

    handle undefined in get truncated address

--- src/utils/addressUtils.ts
+++ src/utils/addressUtils.ts
@@ -1,3 +1,7 @@
-export function getTruncatedAddress(addr: string) {
+// need to keep type as undefined because hyperplay-dev create quests form sometimes passes undefined
+export function getTruncatedAddress(addr?: string) {
+  if (addr === undefined) {
+    return ''
+  }
   return addr.slice(0, 5) + '...' + addr.slice(-5)
 }
