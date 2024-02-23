083bd5cf9f9173d569a8365433de54ca73144aae
Elio Briceño <eliobricenov@gmail.com>
2024-02-23 19:28:09 -0400

    refactor: export modal props correctly

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,3 +1,4 @@
 export { default as ModalAnimation } from './ModalAnimation'
 export { default as ModalSuccess } from './ModalSuccess'
 export { default as Modal } from './Modal'
+export type { ModalProps } from './Modal'
