d959f841b65f4e662d61e8b5010284e1eaff6d52
Redeemer Pace | Goku | Game Dev <75917402+red-game-dev@users.noreply.github.com>
2024-02-26 18:16:59 -0500

    Feat: Integrated loading and error state subscription modal (#217)
    * chore: Updated the naming convetion of for actions container
    
    * feat: Added aria-label to the loading component for testing
    
    * feat: Added loading and error state
    
    * chore: Updated the storybook with latest error and loading state
    
    * fix: Fixed formatting issues

--- src/components/UpdatesSubscriptionModal/UpdatesSubscriptionModal.stories.tsx
+++ src/components/UpdatesSubscriptionModal/UpdatesSubscriptionModal.stories.tsx
@@ -3,15 +3,18 @@ import { expect, fn, userEvent, within } from '@storybook/test'
 
 import UpdatesSubscriptionModal, { defaultI18n } from './index'
 
+const args = {
+  isOpen: true,
+  onSubmit: fn(),
+  onCancel: fn(),
+  onClose: fn(),
+  i18n: defaultI18n
+}
+
 const meta: Meta<typeof UpdatesSubscriptionModal> = {
   title: 'UpdatesSubscriptionModal',
   component: UpdatesSubscriptionModal,
-  args: {
-    isOpen: true,
-    onSubmitClick: fn(),
-    onCancelClick: fn(),
-    i18n: defaultI18n
-  }
+  args
 }
 
 export default meta
@@ -20,58 +23,86 @@ type Story = StoryObj<typeof UpdatesSubscriptionModal>
 
 export const Default: Story = {
   args: {
-    onSubmitClick: () => {
+    onSubmit: () => {
       alert('Thank you for subscribing')
     },
-    onCancelClick: () => {
+    onCancel: () => {
       alert('Cancelled')
+    },
+    onClose: () => {
+      alert('Closed')
     }
   }
 }
 
 export const Valid: Story = {
   play: async ({ canvasElement, args }) => {
-    const onSubmitClick = args.onSubmitClick
+    const onSubmit = args.onSubmit
     const canvas = within(canvasElement)
-    const emailInput = canvas.getByLabelText('Email')
+    const emailInput = canvas.getByPlaceholderText(defaultI18n.inputPlaceholder)
     await userEvent.type(emailInput, 'red.pace.dev@gmail.com')
     await userEvent.click(
       canvas.getByRole('button', {
         name: new RegExp(defaultI18n.submitButtonLabel, 'i')
       })
     )
-    await expect(onSubmitClick).toHaveBeenCalledOnce()
+    await expect(onSubmit).toHaveBeenCalledOnce()
   }
 }
 
 export const Invalid: Story = {
   play: async ({ canvasElement, args }) => {
-    const onSubmitClick = args.onSubmitClick
-    const onCancelClick = args.onCancelClick
+    const onSubmit = args.onSubmit
+    const onCancel = args.onCancel
     const canvas = within(canvasElement)
-    const emailInput = canvas.getByLabelText('Email')
+    const emailInput = canvas.getByPlaceholderText(defaultI18n.inputPlaceholder)
     await userEvent.type(emailInput, 'red.pace.dev')
     await userEvent.click(
       canvas.getByRole('button', {
         name: new RegExp(defaultI18n.submitButtonLabel, 'i')
       })
     )
-    await expect(onSubmitClick).not.toHaveBeenCalled()
-    await expect(onCancelClick).not.toHaveBeenCalled()
+    await expect(onSubmit).not.toHaveBeenCalled()
+    await expect(onCancel).not.toHaveBeenCalled()
   }
 }
 
 export const Cancel: Story = {
   play: async ({ canvasElement, args }) => {
-    const onCancelClick = args.onCancelClick
+    const onCancel = args.onCancel
     const canvas = within(canvasElement)
-    const emailInput = canvas.getByLabelText('Email')
+    const emailInput = canvas.getByPlaceholderText(defaultI18n.inputPlaceholder)
     await userEvent.type(emailInput, 'red.pace.dev@gmail.com')
     await userEvent.click(
       canvas.getByRole('button', {
         name: new RegExp(defaultI18n.cancelButtonLabel, 'i')
       })
     )
-    await expect(onCancelClick).toHaveBeenCalledOnce()
+    await expect(onCancel).toHaveBeenCalledOnce()
+  }
+}
+
+export const Loading: Story = {
+  args: {
+    ...args,
+    loading: true
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await expect(canvas.getByLabelText('Loading')).toBeInTheDocument()
+  }
+}
+
+export const Error: Story = {
+  args: {
+    ...args,
+    error: 'Invalid Email'
+  },
+  play: async ({ canvasElement, args }) => {
+    const error = args.error || ''
+    const canvas = within(canvasElement)
+    const errorText = canvas.queryByText(error)
+    await expect(errorText).toBeVisible()
   }
 }
