4990b3d411f044443a90e4b55af0b5c82bc55f0c
Elio Briceño <eliobricenov@gmail.com>
2024-03-12 13:58:37 -0400

    feat: add custom styles to text area

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,14 +1,32 @@
-import { Textarea as MantineTextarea, TextareaProps } from '@mantine/core'
+import React from 'react'
+
+import {
+  Textarea as MantineTextarea,
+  TextareaProps as MantineTextareaProps
+} from '@mantine/core'
+import cn from 'classnames'
 
 import styles from './TextArea.module.scss'
 
-export { type TextareaProps } from '@mantine/core'
+export interface TextareaProps extends MantineTextareaProps {
+  size?: 'small' | 'medium' | 'large'
+}
 
-export function Textarea(props: TextareaProps) {
+export function Textarea({
+  classNames,
+  size = 'medium',
+  ...props
+}: TextareaProps) {
+  const propsClassName = (classNames ?? {}) as Record<string, string>
   return (
     <MantineTextarea
       classNames={{
-        input: styles.input
+        ...classNames,
+        root: cn(styles.root, propsClassName?.root),
+        input: cn(styles.input, propsClassName?.input, styles[size]),
+        wrapper: cn(styles.wrapper, propsClassName?.wrapper),
+        label: cn('caption', styles.label, propsClassName?.label),
+        section: cn(styles.section, propsClassName?.section)
       }}
       {...props}
     />
