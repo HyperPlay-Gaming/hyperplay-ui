6b1f0343009ff151d9ec74b0221329ef40a282dc
Red <red.pace.dev@gmail.com>
2024-03-13 22:33:06 +0100

    chore: Fixed lint issues

--- src/index.ts
+++ src/index.ts
@@ -144,7 +144,4 @@ export {
 } from './components/UpdatesSubscriptionModal'
 export { default as LayoutTabs } from './components/LayoutTabs'
 export { default as Select } from './components/Select'
-export {
-  default as Collapse,
-  type CollapseProps
-} from './components/Collapse'
+export { default as Collapse, type CollapseProps } from './components/Collapse'
