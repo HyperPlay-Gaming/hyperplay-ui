333822350b42c9d8ee83e33854442b441e180ea0
Red <red.pace.dev@gmail.com>
2024-03-13 21:05:59 +0100

    feat: Added storrybook tests

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,14 +1,44 @@
-import Accordion from '.'
+import Accordion, { AccordionProps } from '.'
+import { expect } from '@storybook/jest'
+import type { Meta, StoryObj } from '@storybook/react'
+import { userEvent, within } from '@storybook/testing-library'
 
-export default {
+type Component = AccordionProps
+type Story = StoryObj<Component>
+
+const meta: Meta<typeof Accordion> = {
   title: 'Accordion',
   component: Accordion
 }
 
-export const Default = () => {
-  return (
-  <Accordion title="Advanced Options">
-    This feature is recommended for advanced users and developers. Learn more
-  </Accordion>
- )
+export default meta;
+
+const textChildren = 'This feature is recommended for advanced users and developers. Learn more';
+
+const props: AccordionProps = {
+  title: 'Advanced Options',
+  children: textChildren
+} 
+
+export const Default: Story = {
+  args: { ...props },
+}
+
+export const OpenedAccordion: Story = {
+  args: { ...props },
+  render: () => (
+    <div>
+      <Accordion title="Advanced Options">
+        This feature is recommended for advanced users and developers. Learn more
+      </Accordion>
+    </div>
+  ),
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const toggleFirstItem = canvas.getByText(props.title)
+    await userEvent.click(toggleFirstItem)
+
+    expect(canvas.getByText(textChildren)).toBeVisible()
+  }
 }
