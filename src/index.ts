0eaf4ac036595359d18660f91d5d6f69c7da3733
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2024-03-25 21:18:55 -0400

    chore: export reward card (#241)

--- src/index.ts
+++ src/index.ts
@@ -155,3 +155,6 @@ export type { RewardERC1155Props } from './components/RewardsSummary/components/
 
 export { default as Menu } from './components/Menu'
 export type { MenuProps } from './components/Menu'
+
+export { default as RewardFormCard } from './components/RewardFormCard'
+export type { RewardFormCardProps } from './components/RewardFormCard'
