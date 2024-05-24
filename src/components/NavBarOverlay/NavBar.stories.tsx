e21dd88b8e4e82bb754a041d83a88e1262551f1f
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-05-23 18:02:40 -0700

    add initial styling

--- src/components/NavBarOverlay/NavBar.stories.tsx
+++ src/components/NavBarOverlay/NavBar.stories.tsx
@@ -1,6 +1,9 @@
 import { HashRouter } from 'react-router-dom'
 
 import type { Meta, StoryObj } from '@storybook/react'
+import { IconExclamationMark } from '@tabler/icons-react'
+
+import { QuestIcon, TrophyOutline } from '@/assets/images'
 
 import { NavBarOverlay, NavBarOverlayProps } from '.'
 
@@ -14,7 +17,20 @@ export default meta
 type Story = StoryObj<typeof NavBarOverlay>
 
 const props: NavBarOverlayProps = {
-  items: [{ title: 'Quests', route: '/quests' }]
+  currentRoute: '/quests',
+  items: [
+    {
+      title: 'Quests',
+      route: '/quests',
+      icon: <QuestIcon fill="white" />,
+      alertIcon: <IconExclamationMark color="yellow" />
+    },
+    {
+      title: 'Achievements (soon)',
+      route: '/achievements',
+      icon: <TrophyOutline fill="white" />
+    }
+  ]
 }
 
 export const Default: Story = {
--- src/components/NavBarOverlay/index.tsx
+++ src/components/NavBarOverlay/index.tsx
@@ -1,20 +1,63 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import { Link } from 'react-router-dom'
+
+import cn from 'classnames'
+
+import { HyperPlayLogoIcon, HyperPlayTextLogo } from '@/assets/images'
+
+import styles from './NavBar.module.scss'
 
 interface NavItem {
   title: string
   route: string
+  icon: ReactElement
+  alertIcon?: ReactElement
 }
 
 export interface NavBarOverlayProps {
   items: NavItem[]
+  currentRoute?: string
+  classNames?: {
+    root?: string
+    link?: string
+    alertIconContainer?: string
+  }
 }
 
-export function NavBarOverlay({ items }: NavBarOverlayProps) {
-  const linkItems = items.map((val) => (
-    <Link to={val.route} key={val.route}>
-      {val.title}
-    </Link>
-  ))
-  return <div>{linkItems}</div>
+export function NavBarOverlay({
+  items,
+  currentRoute,
+  classNames
+}: NavBarOverlayProps) {
+  const linkItems = items.map((val) => {
+    const linkClasses: Record<string, boolean> = {}
+    linkClasses[styles.selected] = currentRoute === val.route
+    return (
+      <Link
+        to={val.route}
+        key={val.route}
+        className={cn('menu', styles.link, classNames?.link, linkClasses)}
+      >
+        {val.icon}
+        {val.title}
+        <div
+          className={cn(
+            styles.alertIconContainer,
+            classNames?.alertIconContainer
+          )}
+        >
+          {val?.alertIcon}
+        </div>
+      </Link>
+    )
+  })
+  return (
+    <div className={cn(styles.root, classNames?.root)}>
+      <div className={styles.logoContainer}>
+        <HyperPlayLogoIcon />
+        <HyperPlayTextLogo />
+      </div>
+      {linkItems}
+    </div>
+  )
 }
