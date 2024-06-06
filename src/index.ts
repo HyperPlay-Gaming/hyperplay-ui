4435e1fedc2737931fdc223468262131b3c41ba1
Red <red.pace.dev@gmail.com>
2024-06-06 21:34:32 +0200

    fix: Added missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import 'react-loading-skeleton/dist/skeleton.css'
+import 'react-loading-skeleton/dist/skeleton.css';
 
-import './fonts.css'
-import './index.scss'
+
+
+import './fonts.css';
+import './index.scss';
+
 
 export { default as TransactionToast } from './components/TransactionToasts'
 export type { TransactionToastProps } from './components/TransactionToasts'
