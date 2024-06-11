a377f932dfb07011ca3b6ad3cc9bb4059bdbd221
Redeemer Pace | Goku | Game Dev <75917402+red-game-dev@users.noreply.github.com>
2024-06-11 15:53:41 +0200

    Feat: Banner component - Website Landing Page (#273)
    * feat: Added assets for the banner
    
    * fix: Fixed missing icons
    
    * feat: Added banner component
    
    * feat: Added banner story & tests
    
    * feat: Button for color direction
    
    * fix: Fixed formatting
    
    * chore: Updated the banner subtitle to the new requested color
    
    * fix: Fixed gradient transition
    
    * fix: Removed outline of the button for gradient
    
    * fix: Fixed formatting
    
    * fix: Moved from utlizing box-shadow inset to mask-image
    
    * chore: Exporetd default Banner component
    
    * fix: Updated colorDirection to be utlized from `style` prop for button
    
    * chore: Update package version to v1.5.7

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,86 @@
+import cn from 'classnames'
+
+import hpClientImg from '@/assets/banners/HyperPlayClient.png?url'
+import { ArrowUpRight } from '@/assets/images'
+import Button from '@/components/Button'
+
+import styles from './Banner.module.scss'
+
+interface BannerI18nProp {
+  title: string
+  subtitle: string
+  description: string
+  submitGame: string
+  installHp: string
+}
+
+export const defaultI18n: BannerI18nProp = {
+  title: 'Freedom to Game',
+  subtitle: 'One App. Every Store.',
+  description:
+    'Access a vast universe of games from top stores, including HyperPlay Store, Epic Games, and GOG all within a unified app.',
+  submitGame: 'Submit a game',
+  installHp: 'Install HyperPlay'
+}
+
+export interface BannerProps {
+  className?: string
+  contentClassName?: string
+  i18n?: BannerI18nProp
+  onTapInstall: () => void
+  onTapSubmitGame: () => void
+}
+
+export const Banner = ({
+  className,
+  contentClassName,
+  onTapInstall,
+  onTapSubmitGame,
+  i18n = defaultI18n
+}: BannerProps) => {
+  return (
+    <div className={cn(styles.root, className)}>
+      <div className={cn(styles.content, contentClassName)}>
+        <div className={styles.contextText}>
+          <p className={styles.bannerSubtitle}>{i18n.subtitle}</p>
+          <h1 className={styles.bannerTitle}>{i18n.title}</h1>
+          <p className={styles.bannerDescription}>{i18n.description}</p>
+        </div>
+        <div className={styles.actionsList}>
+          <Button
+            className={cn(styles.submitGameButton, styles.bannerButton)}
+            type="secondaryGradient"
+            size="medium"
+            spacing="xs"
+            style={
+              {
+                '--color-direction': 'to right'
+              } as React.CSSProperties
+            }
+            rightIcon={<ArrowUpRight className={styles.submitGameButtonIcon} />}
+            onClick={onTapInstall}
+          >
+            {i18n.submitGame}
+          </Button>
+          <Button
+            className={cn(styles.installButton, styles.bannerButton)}
+            size="medium"
+            type="secondary"
+            onClick={onTapSubmitGame}
+          >
+            {i18n.installHp}
+          </Button>
+        </div>
+      </div>
+      <div className={styles.banner}>
+        <img
+          src={hpClientImg}
+          alt="HyperPlay Client"
+          className={styles.bannerImage}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Banner
