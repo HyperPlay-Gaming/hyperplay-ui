623eba8e4b5096bb3b4eaef7244af6a7b9c31597
Red <red.pace.dev@gmail.com>
2024-07-10 21:34:30 +0200

    feat: Added Quest Banner

---
+++ src/components/QuestsBanner/QuestsBanner.stories.tsx
@@ -0,0 +1,79 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+
+
+import CreditsCreditsImage from '@/assets/banners/CreditsCredits.png?url';
+import TreasureChstImage from '@/assets/banners/TreasureChst.png?url';
+
+
+
+import { QuestsBanner, QuestsBannerProps } from '.';
+
+
+const props: QuestsBannerProps = {
+  classNames: {},
+  list: [
+    {
+      bannerImageUrl: TreasureChstImage,
+      title: 'Introducing Quests',
+      description:
+        'You can now earn rewards for completing Quests on HyperPlay.',
+      buttonText: 'View Quests',
+      onButtonTap: () => alert('View Quests clicked!')
+    },
+    {
+      bannerImageUrl: CreditsCreditsImage,
+      title: 'Introducing Game7 Credits',
+      description:
+        'You can now earn G7 Credits for completing Quests on HyperPlay.',
+      buttonText: 'View Quests',
+      onButtonTap: () => alert('View Quests clicked!')
+    }
+  ],
+  totalPages: 2,
+  onCloseButtonTap: () => console.log('Close button clicked!'),
+  onPageChangeTap: (pageIndex: number) =>
+    console.log(`Page changed to ${pageIndex}`)
+}
+
+const meta: Meta<typeof QuestsBanner> = {
+  title: 'Store/QuestsBanner',
+  component: QuestsBanner,
+  args: props
+}
+
+export default meta
+
+type Story = StoryObj<typeof QuestsBanner>
+
+export const Default: Story = {
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'desktop'
+    }
+  },
+  render: (args) => (
+    <div style={{ display: 'flex', width: '100%', padding: '3rem 4rem'}}>
+      <QuestsBanner {...args} />
+    </div>
+  )
+}
+
+export const Smartphone: Story = {
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'mobile2'
+    }
+  }
+}
+
+export const Tablet: Story = {
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'tablet'
+    }
+  }
+}
---
+++ src/components/QuestsBanner/index.tsx
@@ -0,0 +1,157 @@
+import { useState } from 'react'
+
+import { Carousel } from '@mantine/carousel'
+import cn from 'classnames'
+import { EmblaCarouselType } from 'embla-carousel-react'
+
+import { CloseModalIcon, Line } from '@/assets/images'
+import Button from '@/components/Button'
+
+import styles from './QuestsBanner.module.scss'
+
+export interface QuestsBannerSlideItemProp {
+  bannerImageUrl: string
+  title: string
+  description: string
+  buttonText: string
+  onButtonTap: () => void
+}
+
+export interface QuestsBannerProps {
+  classNames?: {
+    root?: string
+    content?: string
+    closeButton?: string
+    slideContainer?: string
+    slide?: string
+    slideItem?: string
+    bannerImage?: string
+    bannerImageContainer?: string
+    details?: string
+    contentText?: string
+    bannerTitle?: string
+    bannerDescription?: string
+    bannerButton?: string
+    paginitionContainer?: string
+    indicator?: string
+    activateIndicator?: string
+  }
+  list?: QuestsBannerSlideItemProp[]
+  totalPages: number
+  onCloseButtonTap: () => void
+  onPageChangeTap: (pageIndex: number) => void
+}
+
+export const QuestsBanner = ({
+  classNames,
+  list = [],
+  totalPages,
+  onCloseButtonTap,
+  onPageChangeTap
+}: QuestsBannerProps) => {
+  const [currentPage, setCurrentPage] = useState(0)
+  const [emblaApiRef, setEmblaApiRef] = useState<EmblaCarouselType>()
+
+  const handlePageChange = (pageIndex: number) => {
+    emblaApiRef?.scrollTo(pageIndex)
+    setCurrentPage(pageIndex)
+    onPageChangeTap(pageIndex)
+  }
+
+  return (
+    <div className={cn(styles.root, classNames?.root)}>
+      <CloseModalIcon 
+        className={cn(styles.closeButton, classNames?.closeButton)}
+        onClick={onCloseButtonTap} 
+      />
+      <div className={cn(styles.bannerImageContainer, classNames?.bannerImageContainer)}>
+          {list[currentPage] && list[currentPage].bannerImageUrl ? (
+            <img
+              src={list[currentPage].bannerImageUrl}
+              className={cn(styles.bannerImage, classNames?.bannerImage)}
+            />
+          ) : null}
+      </div>
+      <div className={cn(styles.content, classNames?.content)}>
+        <Carousel
+          getEmblaApi={(embla) => setEmblaApiRef(embla)}
+          classNames={{
+            slide: cn(styles.slide, classNames?.slide)
+          }}
+          onSlideChange={(index) => {
+            setCurrentPage(index)
+          }}
+          withControls={false}
+          withIndicators={false}
+        >
+          {list?.map(
+            ({ title, description, buttonText, onButtonTap }, index) => (
+              <Carousel.Slide
+                key={index}
+                className={cn(
+                  styles.slideItem,
+                  classNames?.slideItem,
+                  index === currentPage ? styles.active : styles.inactive
+                )}
+              >
+                <div className={cn(styles.details, classNames?.details)}>
+                  <div
+                    className={cn(styles.contentText, classNames?.contentText)}
+                  >
+                    <h2
+                      className={cn(
+                        styles.bannerTitle,
+                        classNames?.bannerTitle
+                      )}
+                    >
+                      {title}
+                    </h2>
+                    <div
+                      className={cn(
+                        styles.bannerDescription,
+                        classNames?.bannerDescription
+                      )}
+                    >
+                      {description}
+                    </div>
+                  </div>
+                  <Button
+                    type="secondary"
+                    onClick={onButtonTap}
+                    className={cn(
+                      styles.bannerButton,
+                      classNames?.bannerButton
+                    )}
+                  >
+                    {buttonText}
+                  </Button>
+                </div>
+              </Carousel.Slide>
+            )
+          )}
+        </Carousel>
+        <div
+          className={cn(
+            styles.paginitionContainer,
+            classNames?.paginitionContainer
+          )}
+        >
+          {Array.from({ length: totalPages }).map((_, index) => (
+            <Line
+              onClick={() => handlePageChange(index)}
+              className={cn(
+                styles.line,
+                classNames?.indicator,
+                index === currentPage ? styles.activateIndicator : '',
+                index === currentPage ? classNames?.activateIndicator : ''
+              )}
+              key={`quests-banner-pagination-${index}`}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default QuestsBanner
