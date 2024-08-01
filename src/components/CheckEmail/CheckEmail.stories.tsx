23e965355ed4b9672f6adda5b3fb71e5e56bd313
Elio Briceño <eliobricenov@gmail.com>
2024-08-01 19:06:08 -0400

    tech(check-email): remove unused import

--- src/components/CheckEmail/CheckEmail.stories.tsx
+++ src/components/CheckEmail/CheckEmail.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import { Meta, StoryObj } from '@storybook/react'
 import { expect, fn, userEvent, within } from '@storybook/test'
