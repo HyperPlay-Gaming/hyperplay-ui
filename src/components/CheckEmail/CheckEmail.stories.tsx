d25a2da88a272e94ae8ecd98abbff4146e0f408b
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2024-08-02 17:08:59 -0400

    Revert "feat(check-email): add support for manual code input (#362)" (#366)
    * Revert "feat(check-email): add support for manual code input (#362)"
    
    This reverts commit 32aee4ce40c76a20a0976a826be8f2c1a99df115.
    
    * chore: bump version

--- src/components/CheckEmail/CheckEmail.stories.tsx
+++ src/components/CheckEmail/CheckEmail.stories.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 import { Meta, StoryObj } from '@storybook/react'
 import { expect, fn, userEvent, within } from '@storybook/test'
 
-import { CheckmarkCircleOutline, XCircle } from '@/assets/images'
-import Loading from '@/components/Loading'
-
 import CheckEmailModal from './index'
 
 const meta: Meta<typeof CheckEmailModal> = {
@@ -32,10 +29,14 @@ export const Default: Story = {
     const onResend = args.onResend
     const onReEnterEmail = args.onReEnterEmail
     const canvas = within(canvasElement)
-    await userEvent.click(canvas.getByRole('button', { name: /resend email/i }))
+    await userEvent.click(
+      canvas.getByRole('button', { name: /click to resend/i })
+    )
     await expect(onResend).toHaveBeenCalled()
     await expect(canvas.getByText(/retry in/i)).toBeInTheDocument()
-    await userEvent.click(canvas.getByRole('button', { name: /change email/i }))
+    await userEvent.click(
+      canvas.getByRole('button', { name: /Re-enter email your email/i })
+    )
     await expect(onReEnterEmail).toHaveBeenCalled()
   }
 }
@@ -49,57 +50,12 @@ export const Timeout: Story = {
   play: async ({ canvasElement, args }) => {
     const onResend = args.onResend
     const canvas = within(canvasElement)
-    await userEvent.click(canvas.getByRole('button', { name: /resend email/i }))
+    await userEvent.click(
+      canvas.getByRole('button', { name: /click to resend/i })
+    )
     await expect(canvas.getByText(/retry in/i)).toBeInTheDocument()
     await userEvent.click(canvas.getByText(/retry in/i))
     await userEvent.click(canvas.getByText(/retry in/i))
     await expect(onResend).toHaveBeenCalledOnce()
   }
-}
-
-export const WithLoadingCodeVerification: Story = {
-  args: {
-    codeInputProps: {
-      disabled: true,
-      rightSection: <Loading style={{ width: 20, height: 20 }} />
-    }
-  },
-  render: (args) => (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <CheckEmailModal {...args} />
-    </div>
-  )
-}
-
-export const WithSuccessCode: Story = {
-  args: {
-    codeInputProps: {
-      rightSection: (
-        <CheckmarkCircleOutline
-          fill="var(--color-success-400)"
-          width={20}
-          height={20}
-        />
-      )
-    }
-  },
-  render: (args) => (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <CheckEmailModal {...args} />
-    </div>
-  )
-}
-
-export const WithErrorCode: Story = {
-  args: {
-    codeInputProps: {
-      error: 'Verification failed. Please try again.',
-      rightSection: <XCircle width={20} height={20} />
-    }
-  },
-  render: (args) => (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <CheckEmailModal {...args} />
-    </div>
-  )
 }
--- src/components/CheckEmail/index.tsx
+++ src/components/CheckEmail/index.tsx
@@ -2,11 +2,9 @@ import React, { HTMLProps, useEffect, useState } from 'react'
 
 import cn from 'classnames'
 
-import { HyperPlayLogoIcon } from '@/assets/images'
+import { Email } from '@/assets/images'
 import Button from '@/components/Button'
-import Collapse from '@/components/Collapse'
 import Modal from '@/components/Modal/Modal'
-import TextInput, { TextInputProps } from '@/components/TextInput'
 
 import styles from './CheckEmail.module.scss'
 
@@ -15,19 +13,14 @@ export interface CheckEmailProps extends HTMLProps<HTMLDivElement> {
   onClose: () => void
   onResend: () => void
   onReEnterEmail: () => void
-  codeInputProps?: TextInputProps
   i18n?: {
     title: string
     subtitle: string
-    clickLoginTitleOr: string
-    redirectNotWorking: string
     didNotReceiveEmail: string
     resend: string
     retryIn: string
     seconds: string
     reEnterEmail: string
-    manualCodeTitle: string
-    inputPlaceHolder: string
   }
 }
 
@@ -36,20 +29,15 @@ const CheckEmail = ({
   email,
   onResend,
   onReEnterEmail,
-  codeInputProps,
   onClose,
   i18n = {
     title: 'Check your email',
-    subtitle: "We've sent a verification link to",
-    clickLoginTitleOr: 'Click the link to login or',
-    didNotReceiveEmail: `Didn't get an email?`,
-    redirectNotWorking: 'Redirect not working?',
-    resend: 'Resend email',
+    subtitle: 'We sent a verification link to',
+    didNotReceiveEmail: `Didn't receive an email?`,
+    resend: 'Click to resend',
     retryIn: 'Retry in',
     seconds: 'seconds',
-    reEnterEmail: 'change email',
-    manualCodeTitle: 'Login Code',
-    inputPlaceHolder: 'Paste login code'
+    reEnterEmail: 'Re-enter email your email'
   },
   ...props
 }: CheckEmailProps) => {
@@ -78,38 +66,21 @@ const CheckEmail = ({
     <Modal.Root className={cn(className, styles.root)} {...props}>
       <Modal.CloseButton aria-label="close signup modal" onClick={onClose} />
       <Modal.HeadingIcon className={styles.emailRoundedIcon}>
-        <HyperPlayLogoIcon className={styles.icon} width={20} height={20} />
+        <Email className={styles.icon} width={20} height={20} />
       </Modal.HeadingIcon>
       <Modal.Header>
         <Modal.Title>{i18n.title}</Modal.Title>
-        <Modal.Body className={'body-sm'}>
-          {i18n.subtitle} <span className="text--semibold">{email}</span>.{' '}
-          {i18n.clickLoginTitleOr}{' '}
-          <Button
-            type="link"
-            className={styles.reEnter}
-            onClick={onReEnterEmail}
-          >
-            {i18n?.reEnterEmail}.
-          </Button>
+        <Modal.Body>
+          {i18n.subtitle} <span className="text--semibold">{email}</span>
         </Modal.Body>
-      </Modal.Header>
-      <div className={styles.copyCodeContainer}>
-        <span className="eyebrow">{i18n.redirectNotWorking}</span>
-        <Collapse
-          title={i18n.manualCodeTitle}
-          classNames={{
-            root: styles.collapseRoot,
-            content: styles.collapseContent
-          }}
+        <Button
+          type="tertiary"
+          className={styles.reEnter}
+          onClick={onReEnterEmail}
         >
-          <TextInput
-            className={styles.input}
-            placeholder={i18n.inputPlaceHolder}
-            {...codeInputProps}
-          />
-        </Collapse>
-      </div>
+          {i18n?.reEnterEmail}
+        </Button>
+      </Modal.Header>
       <div className={styles.linkContainer}>
         <span className={cn('button-sm', styles.subtitle)}>
           {i18n.didNotReceiveEmail}
