8c29a8a29a24a9e90fd72e5f0c81d83813c86968
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-01-08 16:04:33 -0300

    feat(quest-list): refactor QuestList component to simplify props and enhance readability

--- src/components/QuestList/index.tsx
+++ src/components/QuestList/index.tsx
@@ -1,156 +1,131 @@
-import { HTMLProps } from 'react'
-
-import { Quest as BaseQuest } from '@hyperplay/utils'
 import classNames from 'classnames'
-
+import { QuestListProps } from './types'
 import { Refresh } from '@/assets/images'
-import { ReactComponent as Lightning } from '@/assets/images/lightning-02.svg'
-import { ReactComponent as QuestIconTwo } from '@/assets/images/quest-icon-2.svg'
-import { ReactComponent as QuestIcon } from '@/assets/images/quest-icon.svg'
-import { QuestType } from '@/common/types'
+import { ReactComponent as Lightning } from '@/assets/images/LightningOutlined.svg'
+import { ReactComponent as QuestIconTwo } from '@/assets/images/QuestIcon2.svg'
+import { ReactComponent as QuestIcon } from '@/assets/images/QuestIcon.svg'
 import Button from '@/components/Button'
-
 import Sticker from '@/components/Sticker'
 import styles from './index.module.scss'
 
-interface Quest extends BaseQuest {
-  state: 'READY_FOR_CLAIM' | 'ACTIVE'
-}
+export default function QuestList({ i18n }: QuestListProps) {
+  const readyForClaimQuests = [
+    {
+      id: 1,
+      title: 'Play Streak',
+      duration: '0 / 7 days',
+      resetTime: 'Day resets:',
+      time: '23:43:03 (UTC)',
+      questType: 'PLAYSTREAK',
+      reward: '+200 G7C'
+    }
+  ]
+  const activeQuests = [
+    {
+      id: 2,
+      title: 'Play Streak',
+      duration: '0 / 7 days',
+      resetTime: 'Day resets:',
+      time: '23:43:03 (UTC)',
+      questType: 'PLAYSTREAK',
+      reward: '+200 G7C'
+    }
+  ]
 
-export interface QuestListProps extends Quest {
-  onClick: () => void
-  selected?: boolean
-  state: 'READY_FOR_CLAIM' | 'ACTIVE'
-  className?: string
-  i18n?: {
-    quests: string
-    viewAllButton: string
-  }
-  quests: Quest[]
-}
-
-export default function QuestList({
-  selected,
-  i18n,
-  quests,
-  onClick,
-  className,
-  state,
-  ...props
-}: QuestListProps) {
-   
-    const activeQuests = quests.filter((val) => val.state === 'ACTIVE')
-
-    const readyForClaimQuests = quests.filter(
-      (val) => val.state === 'READY_FOR_CLAIM'
-    )
-
-    return (
-      <div className={classNames(styles.component)}>
-        <div className={classNames(styles.header)}>
-          <p className={classNames('title', styles.questTitle)}>
-            {i18n?.quests}
-          </p>
-          <Button
-            type="link"
-            size="small"
-            htmlType="button"
-            className={styles.viewAllButton}
-          >
-            {i18n?.viewAllButton}
-          </Button>
-        </div>
-
-        <div className={classNames(styles.questList)}>
-          {/* Ready for Claim */}
-          <div className={classNames(styles.readyForClaim)}>
-            <p className={classNames(styles.readyForClaimTitle)}>
-              <QuestIcon className={classNames(styles.QuestIcon)} />
-              Ready for Claim
-            </p>
-            <Sticker styleType="secondary" variant="filledStrong">
-              Label
-            </Sticker>
-          </div>
-
-          {readyForClaimQuests.map((quest) => (
-            <div key={quest.id} className={classNames(styles.card)}>
-              <div className={classNames(styles.firstRow)}>
-                <p className={classNames('title', styles.days)}>0 / 7 days</p>
-                <div className={classNames(styles.badges)}>
-                  <p className={classNames('body', styles.badgesSpace)}>
-                    <Sticker styleType="tertiary" variant="outlined">
-                      <Refresh className={classNames(styles.badgeIcon)} />
-                      Weekly
-                    </Sticker>
-                    <Sticker styleType="tertiary" variant="outlined">
-                      2x
-                    </Sticker>
-                  </p>
-                  <p className={classNames('body', styles.resetTime)}>
-                    Day resets:{' '}
-                    <span className={classNames(styles.time)}>
-                      23:43:03 (UTC)
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <div className={classNames(styles.secondRow)}>
-                <p className={classNames('body', styles.questType)}>
-                  <Lightning className={classNames(styles.lightningIcon)} />
-                  Play Streak
-                </p>
-                <p className={classNames('body', styles.rewardType)}>
-                  +200 G7C
-                </p>
-              </div>
-            </div>
-          ))}
-
-          {/* Active Quests */}
-          <div className={classNames(styles.activeQuests)}>
-            <p className={classNames(styles.activeQuestsTitle)}>
-              <QuestIconTwo className={classNames(styles.QuestIcoTwo)} /> Active
-            </p>
-            <Sticker styleType="secondary" variant="filledStrong">
-              Label
-            </Sticker>
-          </div>
-          {activeQuests.map((quest) => (
-            <div key={quest.id} className={classNames(styles.card)}>
-              <div className={classNames(styles.firstRow)}>
-                <p className={classNames('title', styles.days)}>0 / 7 days</p>
-                <div className={classNames(styles.badges)}>
-                  <p className={classNames('body', styles.badgesSpace)}>
-                    <Sticker styleType="tertiary" variant="outlined">
-                      <Refresh className={classNames(styles.badgeIcon)} />
-                      Weekly
-                    </Sticker>
-                    <Sticker styleType="tertiary" variant="outlined">
-                      2x
-                    </Sticker>
-                  </p>
-                  <p className={classNames('body', styles.resetTime)}>
-                    Day resets:{' '}
-                    <span className={classNames(styles.time)}>
-                      23:43:03 (UTC)
-                    </span>
-                  </p>
-                </div>
-              </div>
-              <div className={classNames(styles.secondRow)}>
-                <p className={classNames('body', styles.questType)}>
-                  <Lightning className={classNames(styles.lightningIcon)} />
-                  Play Streak
-                </p>
-                <p className={classNames('body', styles.rewardType)}>
-                  +200 G7C
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
+  return (
+    <div>
+      <div className={styles.header}>
+        <p className={classNames('title', styles.questTitle)}>Quests</p>
+        <Button
+          type="link"
+          size="small"
+          htmlType="button"
+          className={styles.viewAllButton}
+        >
+          {i18n?.viewAllButton}
+        </Button>
       </div>
-    )
-  }
+
+      <div className={styles.questList}>
+        {/* Ready for Claim */}
+        <div className={styles.readyForClaim}>
+          <p className={styles.readyForClaimTitle}>
+            <QuestIcon className={styles.questIcon} />
+            Ready for Claim
+          </p>
+          <Sticker styleType="secondary" variant="filledStrong">
+            Label
+          </Sticker>
+        </div>
+        {readyForClaimQuests.map((quest) => (
+          <div key={quest.id} className={styles.card}>
+            <div className={styles.firstRow}>
+              <p className={classNames('title', styles.days)}>0 / 7 days</p>
+              <div className={styles.badges}>
+                <p className={styles.badgesSpace}>
+                  <Sticker styleType="tertiary" variant="outlined">
+                    <Refresh className={styles.badgeIcon} />
+                    Weekly
+                  </Sticker>
+                  <Sticker styleType="tertiary" variant="outlined">
+                    2x
+                  </Sticker>
+                </p>
+                <p className={styles.resetTime}>
+                  Day resets:{' '}
+                  <span className={styles.time}>23:43:03 (UTC)</span>
+                </p>
+              </div>
+            </div>
+            <div className={styles.secondRow}>
+              <p className={styles.questType}>
+                <Lightning className={styles.lightningIcon} />
+                Play Streak
+              </p>
+              <p className={styles.rewardType}>+200 G7C</p>
+            </div>
+          </div>
+        ))}
+
+        {/* Active Quests */}
+        <div className={styles.activeQuests}>
+          <p className={styles.activeQuestsTitle}>
+            <QuestIconTwo className={styles.QuestIcoTwo} /> Active
+          </p>
+          <Sticker styleType="secondary" variant="filledStrong">
+            Label
+          </Sticker>
+        </div>
+        {activeQuests.map((quest) => (
+          <div key={quest.id} className={styles.card}>
+            <div className={styles.firstRow}>
+              <p className={classNames('title', styles.days)}>0 / 7 days</p>
+              <div className={styles.badges}>
+                <p className={styles.badgesSpace}>
+                  <Sticker styleType="tertiary" variant="outlined">
+                    <Refresh className={styles.badgeIcon} />
+                    Weekly
+                  </Sticker>
+                  <Sticker styleType="tertiary" variant="outlined">
+                    2x
+                  </Sticker>
+                </p>
+                <p className={styles.resetTime}>
+                  Day resets:{' '}
+                  <span className={styles.time}>23:43:03 (UTC)</span>
+                </p>
+              </div>
+            </div>
+            <div className={styles.secondRow}>
+              <p className={classNames('body', styles.questType)}>
+                <Lightning className={styles.lightningIcon} />
+                Play Streak
+              </p>
+              <p className={styles.rewardType}>+200 G7C</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
