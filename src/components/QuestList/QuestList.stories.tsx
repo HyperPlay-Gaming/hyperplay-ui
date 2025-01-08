61e32f923c222b8a81daae549535c15f8c5529a5
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-01-08 16:04:48 -0300

    feat(quest-list): update Storybook stories to include QuestListProps and integrate Sticker component

--- src/components/QuestList/QuestList.stories.tsx
+++ src/components/QuestList/QuestList.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
-
+import { QuestListProps } from './types'
 import QuestList from './index'
-import QuestListi18n from './index'
+import Sticker from '@/components/Sticker'
 
 const meta: Meta<typeof QuestList> = {
   title: 'Quests/QuestList',
@@ -12,14 +12,22 @@ export default meta
 
 type Story = StoryObj<typeof QuestList>
 
-const i18n: typeof QuestListi18n = {
-  ready: 'Ready',
-  active: 'Active',
-  quests: 'Quests',
-  viewAllButton: 'View all'
+const props: QuestListProps = {
+  i18n: {
+    selected: true,
+    title: 'Quests',
+    duration: '0/7 Days',
+    resetTime: 'Day resets:',
+    time: '23:43:03 (UTC)',
+    questType: 'PLAYSTREAK',
+    reward: '1 Azuki',
+    viewAllButton: 'View All',
+    badgeOne: Sticker,
+    badgeTwo: Sticker,
+    state: 'READY_FOR_CLAIM',
+    quests: []
+  }
 }
-
-const props = { i18n }
 
 export const Default: Story = {
   args: { ...props }
