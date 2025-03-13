69bd749f10a89768e27de1c596946c01e8a89749
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-03-13 00:35:56 +0000

    Update GameAbout Component to include Bagdes and Button Link

--- src/components/GameAbout/index.tsx
+++ src/components/GameAbout/index.tsx
@@ -1,23 +1,41 @@
 import React, { useEffect, useRef, useState } from 'react'
 
+import { IconAlertTriangle, IconInfoCircle } from '@tabler/icons-react'
 import classNames from 'classnames'
 
 import * as Images from '@/assets/images'
+import Button from '@/components/Button'
 
+import Sticker from '../Sticker'
 import styles from './GameAbout.module.scss'
 
 export interface GameAboutProps {
   description: string
   gameName?: string
+  stickers?: Array<{
+    icon: string
+    label: string
+  }>
+  buttonLink?: {
+    label: string
+    onClick: () => void
+  }
   classnames?: {
     container?: string
     title?: string
     description?: string
     gameTitle?: string
+    button?: string
+    stickers?: string
   }
 }
 
-const GameAbout = ({ description, gameName, classnames }: GameAboutProps) => {
+const GameAbout = ({
+  description,
+  gameName,
+  stickers,
+  classnames
+}: GameAboutProps) => {
   const aboutText = useRef<HTMLParagraphElement | null>(null)
   const [isClamped, setIsClamped] = useState(false)
   const [expanded, setExpanded] = useState(false)
@@ -44,6 +62,28 @@ const GameAbout = ({ description, gameName, classnames }: GameAboutProps) => {
         </div>
       )}
       <div className={classNames('caption', classnames?.title)}>About</div>
+      {stickers && stickers.length > 0 && (
+        <div className={classNames(styles.stickers, classnames?.stickers)}>
+          {stickers.map((sticker, index) => (
+            <Sticker
+              key={index}
+              styleType={
+                sticker.label === 'Access Gated' ? 'warning' : 'tertiary'
+              }
+              variant="outlined"
+            >
+              <div className={styles.stickerContent}>
+                {sticker.label === 'Access Gated' ? (
+                  <IconAlertTriangle className={styles.iconBadge} />
+                ) : (
+                  <IconInfoCircle className={styles.iconBadge} />
+                )}
+                <span>{sticker.label}</span>
+              </div>
+            </Sticker>
+          ))}
+        </div>
+      )}
       <p
         ref={aboutText}
         className={classNames(
@@ -56,20 +96,21 @@ const GameAbout = ({ description, gameName, classnames }: GameAboutProps) => {
         {description}
       </p>
       {isClamped && (
-        <button
-          className={classNames('body-sm', styles.readMore)}
+        <Button
+          className={classNames('button-sm', styles.showMore)}
           onClick={() => setExpanded(!expanded)}
+          type="link"
         >
           {expanded ? (
             <>
-              Read less <Images.DownArrow className={styles.iconOpen} />
+              Show less <Images.DownArrow className={styles.iconOpen} />
             </>
           ) : (
             <>
-              Read more <Images.DownArrow />
+              Show more <Images.DownArrow className={styles.iconOpen} />
             </>
           )}
-        </button>
+        </Button>
       )}
     </div>
   )
