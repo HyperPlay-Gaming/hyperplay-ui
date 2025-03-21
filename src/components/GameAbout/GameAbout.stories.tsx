cf1bbda83315c1843daeae95fe171a6cd43bc1c3
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-03-21 20:42:11 +0000

    Adding support for button interactions, including usability testing.

--- src/components/GameAbout/GameAbout.stories.tsx
+++ src/components/GameAbout/GameAbout.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { expect, userEvent, within } from '@storybook/test'
 
 import { AlertTriangle, Info } from '@/assets/images'
 
@@ -15,60 +16,92 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof GameAbout>
 
-const shortDescription = `Shoot, Earn, Pioneer: Now We Live in XOCIETY.
-
-Backed by prominent investors such as HASHED, Spartan, KRAFTON, and Sui. XOCIETY stands as Sui's flagship gaming project.
-
-Set in a deep Sci-Fi world. XOCIETY aims to redefine gaming experiences offering players economic control and transforming them into key decision-makers. With the revolutionary XOCIETY Corporate Share system, players take control of the core parts of XOCIETY, from...`
-
 const longDescription = `Shoot, Earn, Pioneer: Now We Live in XOCIETY.
 
 Backed by prominent investors such as HASHED, Spartan, KRAFTON, and Sui. XOCIETY stands as Sui's flagship gaming project.
 
-Set in a deep Sci-Fi world. XOCIETY aims to redefine gaming experiences offering players economic control and transforming them into key decision-makers. With the revolutionary XOCIETY Corporate Share system, players take control of the core parts of XOCIETY, from tournament modes to wearables, they are in control and rewarded.
+Set in a deep Sci-Fi world. XOCIETY aims to redefine gaming experiences offering players economic control and transforming 
+them into key decision-makers. With the revolutionary XOCIETY Corporate Share system, players take control of the core parts of XOCIETY, 
+from tournament modes to wearables, they are in control and rewarded.
 
-Developed by XOCIETY's 36 veteran developers (avg 10+ experience in gaming) from global AAA studios such as NCSOFT, from tournament modes to wearables, they are in control and rewarded.
+Developed by XOCIETY's 36 veteran developers (avg 10+ experience in gaming) from global AAA 
+studios such as NCSOFT, from tournament modes to wearables, they are in control and rewarded.
 
-Featuring PvP and PvE elements combined with RPG mechanics. With an emphasis on player experience, XOCIETY aimes to redefine gaming experience offering TPS players economic control, and transforming them into key economic agents. Unlike traditional FPS games, where a single user's activity has little impact on the broader game environment, decisions in XOCIETY create dynamic ripple effects throughout the gameplay and environment.`
+Featuring PvP and PvE elements combined with RPG mechanics. With an emphasis on player experience, 
+XOCIETY aimes to redefine gaming experience offering TPS players economic control, and transforming 
+them into key economic agents. Unlike traditional FPS games, where a single user's activity has little 
+impact on the broader game environment, decisions in XOCIETY create dynamic ripple effects throughout 
+the gameplay and environment.`
 
+// Default
 export const Default: Story = {
   args: {
     titleSmall: 'About',
     description: longDescription,
     buttonLink: {
+      expanded: false,
       onClick: () => console.log('Button clicked')
+    },
+    i18n: {
+      showMore: 'Show more',
+      showLess: 'Show less'
     }
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await userEvent.click(canvas.getByTestId('button'), {
+      delay: 1000
+    })
+    expect(canvas.getByTestId('button')).toHaveStyle({
+      display: 'flex'
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 1200))
+    await userEvent.click(canvas.getByTestId('button'), {
+      delay: 1000
+    })
+    expect(canvas.getByTestId('button')).toHaveStyle({
+      display: 'flex'
+    })
   }
 }
 
-export const ShortDescription: Story = {
-  args: {
-    titleSmall: 'About',
-    description: shortDescription,
-    buttonLink: {
-      onClick: () => console.log('Button clicked')
-    }
-  }
-}
-
-export const LongDescription: Story = {
-  args: {
-    titleSmall: 'About',
-    description: longDescription,
-    buttonLink: {
-      expanded: true,
-      onClick: () => console.log('Button clicked')
-    }
-  }
-}
-
+// With Game Name
 export const WithGameName: Story = {
   args: {
     gameName: 'XOCIETY',
-    description: longDescription
+    description: longDescription,
+    buttonLink: {
+      expanded: false,
+      onClick: () => console.log('Button clicked')
+    },
+    i18n: {
+      showMore: 'Show more',
+      showLess: 'Show less'
+    }
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await userEvent.click(canvas.getByTestId('button'), {
+      delay: 1000
+    })
+    expect(canvas.getByTestId('button')).toHaveStyle({
+      display: 'flex'
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 1200))
+    await userEvent.click(canvas.getByTestId('button'), {
+      delay: 1000
+    })
+    expect(canvas.getByTestId('button')).toHaveStyle({
+      display: 'flex'
+    })
   }
 }
 
+// With Stickers and Button
 export const WithStickersAndButton: Story = {
   args: {
     titleLarge: 'About this Game',
@@ -84,7 +117,30 @@ export const WithStickersAndButton: Story = {
       }
     ],
     buttonLink: {
+      expanded: false,
       onClick: () => console.log('Button clicked')
+    },
+    i18n: {
+      showMore: 'Show more',
+      showLess: 'Show less'
     }
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await userEvent.click(canvas.getByTestId('button'), {
+      delay: 1000
+    })
+    expect(canvas.getByTestId('button')).toHaveStyle({
+      display: 'flex'
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 1200))
+    await userEvent.click(canvas.getByTestId('button'), {
+      delay: 1000
+    })
+    expect(canvas.getByTestId('button')).toHaveStyle({
+      display: 'flex'
+    })
   }
 }
