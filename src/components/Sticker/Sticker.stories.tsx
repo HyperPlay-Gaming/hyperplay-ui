ea218b3691b1359eb811e793abb7e331df61b27e
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-03-21 20:39:14 +0000

    Updates the Sticker component examples to use 'Label' as text and implements some properties

--- src/components/Sticker/Sticker.stories.tsx
+++ src/components/Sticker/Sticker.stories.tsx
@@ -19,34 +19,30 @@ export const Default: Story = {
     styleType: 'secondary',
     dimension: 'default',
     variant: 'outlined',
-    children: 'Sticker'
+    children: 'Label'
   }
 }
 
 export const DefaultWithIcon: Story = {
   args: {
     styleType: 'secondary',
-    styleCategory: {
-      variant: 'withIcon',
-      icon: <AlertTriangle data-icon="alert-triangle" />
-    },
-    dimension: 'default',
     variant: 'outlined',
-    children: 'Sticker'
+    withIcon: <AlertTriangle />,
+    dimension: 'default',
+    children: 'Label'
   }
 }
 
 export const DefaultWithDot: Story = {
   args: {
     styleType: 'secondary',
-    styleCategory: {
-      variant: 'withDot',
+    withDot: {
       dotColor: 'success',
-      dotIcon: <DotIcon />
+      dotIcon: DotIcon
     },
     dimension: 'default',
     variant: 'outlined',
-    children: 'Sticker'
+    children: 'Label'
   }
 }
 
@@ -56,33 +52,29 @@ export const Small: Story = {
     styleType: 'secondary',
     dimension: 'small',
     variant: 'outlined',
-    children: 'Sticker'
+    children: 'Label'
   }
 }
 
 export const SmallWithIcon: Story = {
   args: {
     styleType: 'secondary',
-    styleCategory: {
-      variant: 'withIcon',
-      icon: <AlertTriangle data-icon="alert-triangle" />
-    },
+    withIcon: <AlertTriangle />,
     dimension: 'small',
     variant: 'outlined',
-    children: 'Sticker'
+    children: 'Label'
   }
 }
 
 export const SmallWithDot: Story = {
   args: {
     styleType: 'secondary',
-    styleCategory: {
-      variant: 'withDot',
+    withDot: {
       dotColor: 'success',
-      dotIcon: <DotIcon />
+      dotIcon: DotIcon
     },
     dimension: 'small',
     variant: 'outlined',
-    children: 'Sticker'
+    children: 'Label'
   }
 }
