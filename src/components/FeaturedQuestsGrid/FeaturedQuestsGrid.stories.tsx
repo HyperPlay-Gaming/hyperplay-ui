56871331bbeb9be0a518a0f874a3306efef800e8
Elio Briceño <eliobricenov@gmail.com>
2025-03-26 16:39:49 -0400

    feat: Integrate dnd-kit for sortable grid functionality in FeaturedQuestsGrid component

--- src/components/FeaturedQuestsGrid/FeaturedQuestsGrid.stories.tsx
+++ src/components/FeaturedQuestsGrid/FeaturedQuestsGrid.stories.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { rectSortingStrategy } from '@dnd-kit/sortable'
 import { Meta, StoryObj } from '@storybook/react'
 
 import { FeaturedQuestsGrid } from './FeaturedQuestsGrid'
+import { GridContainer } from './GridContainer'
+import { Sortable, Props as SortableProps } from './Sortable'
+import './root.scss'
 
 const meta: Meta<typeof FeaturedQuestsGrid> = {
   title: 'FeaturedQuestsGrid',
@@ -12,3 +17,15 @@ export default meta
 type Story = StoryObj<typeof FeaturedQuestsGrid>
 
 export const Default: Story = {}
+
+const props: Partial<SortableProps> = {
+  adjustScale: true,
+  Container: (props: any) => <GridContainer {...props} columns={5} />,
+  strategy: rectSortingStrategy,
+  wrapperStyle: () => ({
+    width: 140,
+    height: 140
+  })
+}
+
+export const BasicSetup = () => <Sortable {...props} />
