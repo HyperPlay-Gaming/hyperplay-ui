3323a1de8baea5b962caa7739d842c163981b11d
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-04-04 18:19:35 +0200

    [Feat] New GameInfoV2 component (#463)
    * [UI] Refactor QuestCard component to group currency name with avatar
    
    * [UI] Update QuestCard styles for improved layout and alignment
    
    * [Chore] Bump version to 1.9.10 in package.json
    
    * [UI] fix currency name mispelling
    
    * [UI] Add new story example for odd-sized QuestCard
    
    * [UI] Improve QuestCard styles and layout
    
    * [UI] Add maximum height to QuestCard
    
    * [Chore] Bump version to 1.9.12 in package.json
    
    * [UI] Add new GameInfoV2 component
    
    * [UI] AAdd Syles to the New GameInfoV2 component
    
    * [UI] Add Stories nto the different states of the GameInfoV2 Component
    
    * [Chore] Add support for the .jpg files on TypeScript
    
    * [UI] Added new icons: Plus, EditorChoice e LoadingIcon2
    
    * [UI] Add Plus Icon SVG
    
    * [UI] Add EditoChoice Icon SVG
    
    * [UI] Add LoadingIcon2 Icon SVG
    
    * [Chore] resolve package conflicts for the package.json
    
    * Add new blockchain icons
    
    * Refactor to support new icons
    
    * Include new blockchain and social icons
    
    * fix styles and add news classes for the buttons and correct spacements
    
    * Add Globe Icon
    
    * Add X filled icon
    
    * Add Youtube filled icon
    
    * Added new icons: Globe, X filled e YoutubeFilled
    
    * Update DiscordFilled Icon
    
    * Add new Icons for the blockchains
    
    * Refactor styles to adjust spacing amd responsiveness
    
    * Update some paddings for mobile
    
    * Update adding new stories
    
    * Add structure to improve legibility and render the icons
    
    * Bump version to 1.14.1
    
    * fix: icon name
    
    * feat: add Blockchain component
    
    * feat: add BlockchainStack Styles
    
    * feat: add BlockchainStack story and storybook test
    
    * remove: delete unused svg blockchain icons
    
    * remove: add polygon svg unused icon
    
    * feat: add fallback icon for blockchains
    
    * refactor: remove unused icons and add new one
    
    * chore: update @hyperplay/chains to 0.5.0
    
    * chore: update dependencies
    
    * remove: remove unsed icons
    
    * refactor: change fill to be current color value
    
    * remove: delete unused svgs
    
    * feat: add title prop to the component
    
    * feat: refactor component to include new BlockchainStack and MetaSection components
    
    * feat: update stories and add loading state story
    
    * feat: fix social network cions, and impelment shineLoader class
    
    * feat: update pnpm-lock.yaml dependencies
    
    * feat: Add action button for the isLoading story
    
    * feat: change sticker variant to 'filledStrong' after pulling last changes from main
    
    * feat: bump version to 1.16.2
    
    * Revert "feat: update pnpm-lock.yaml dependencies"
    
    This reverts commit ce521b838aa49456e3ed294263ad8b98839682be.
    
    * Revert "chore: update dependencies"
    
    This reverts commit dae485400464ece9ac67a2eb4dd7174dbefccb2f.
    
    * feat: update pnpm-lock.yaml
    
    * Revert "feat: update pnpm-lock.yaml"
    
    This reverts commit dccda9e35f7210abd7ec4fdbde0c30e76df976ac.
    
    * Delete yarn.lock
    
    * feat: update pnpm-lock.yaml
    
    * feat: add new components GameInfoV2 and BlockchainsStack
    
    * fix: avoid rendering null items in meta section
    
    * feat: add internationalization supportand and some fixes
    
    * style: updating styles for background and border colors
    
    * style: refactor styles, removing unused classes and using variables for image dimensions
    
    * feat: add new SocialLinks component
    
    * feat: implement new Social Icons component
    
    * feat: add SocialLinks Stories
    
    * style: add SocialLinks styles
    
    * feat: remove social links and add SocialLinks to GameInfo stories instead
    
    * style: adjust spacement of badges and remove social links styles
    
    * refactor: removed unused function and fix prettier
    
    * [Tech] refactor game info (#479)
    
    * make sociallinks a pseudo component
    
    * add default init to i18n for game info v2
    
    * refactor blockchain to be a react node in game info
    
    * take arbitrary image component as prop
    
    * prettier
    
    * add long game title story
    
    * take ipfsBaseUrl as optional prop
    
    ---------
    
    Co-authored-by: Brett <27568879+BrettCleary@users.noreply.github.com>

---
+++ src/components/BlockchainsStack/index.tsx
@@ -0,0 +1,130 @@
+import React, { HTMLAttributes, PropsWithChildren, useState } from 'react'
+
+import { chainMap } from '@hyperplay/chains'
+import { Popover } from '@mantine/core'
+import classNames from 'classnames'
+
+import BlockchainIconImg from '@/assets/images/BlockchainIconImg.svg?url'
+
+import styles from './BlockchainsStack.module.scss'
+
+const formatIpfsUrl = (ipfsBaseUrl: string, url?: string) => {
+  if (!url) return ''
+  return url.replace('ipfs://', ipfsBaseUrl)
+}
+
+export interface BlockchainsStackProps
+  extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  title?: string
+  chainId?: string[]
+  maxVisible?: number
+  showMoreCount?: boolean
+  className?: string
+  // Next/Image can be passed in here to use instead of img tag
+  /* eslint-disable-next-line */
+  Image?: any
+  ipfsBaseUrl?: string
+}
+
+const BlockchainsStack = ({
+  chainId = [],
+  title = 'BLOCKCHAIN(S):',
+  maxVisible = 5,
+  showMoreCount = true,
+  className,
+  Image = 'img',
+  ipfsBaseUrl = 'https://ipfs.io/ipfs/',
+  ...props
+}: BlockchainsStackProps) => {
+  const [isMoreHovered, setIsMoreHovered] = useState(false)
+  const [missingIcon, setMissingIcon] = useState<Record<string, boolean>>({})
+
+  const blockchains = chainId.map((id) => {
+    const chain = chainMap[id]
+    const iconUrl = chain?.icon?.[0]?.url
+      ? formatIpfsUrl(ipfsBaseUrl, chain.icon[0].url)
+      : ''
+    return {
+      name: chain?.chain.name || id,
+      iconUrl
+    }
+  })
+
+  const handleImageError = (iconUrl: string) => {
+    setMissingIcon((prev) => ({ ...prev, [iconUrl]: true }))
+  }
+
+  const getIconSrc = (iconUrl: string) => {
+    if (!iconUrl || missingIcon[iconUrl]) {
+      return BlockchainIconImg
+    }
+    return iconUrl
+  }
+
+  const visibleBlockchains = blockchains.slice(0, maxVisible)
+  const moreCount = blockchains.length - maxVisible
+  const hasMoreBlockchains = moreCount > 0
+  const remainingBlockchains = blockchains.slice(maxVisible)
+
+  return (
+    <div className={classNames(styles.blockchains, className)} {...props}>
+      <span className={styles.title}>{title}</span>
+      <div className={styles.blockchainIcons}>
+        {visibleBlockchains.map((blockchain, index) => (
+          <div key={`blockchain-${index}`} className={styles.icon}>
+            <Image
+              src={getIconSrc(blockchain.iconUrl)}
+              alt={blockchain.name}
+              className={styles.blockchainSvg}
+              onError={() => handleImageError(blockchain.iconUrl)}
+            />
+          </div>
+        ))}
+        {hasMoreBlockchains && showMoreCount && (
+          <Popover
+            opened={isMoreHovered}
+            position="top"
+            withArrow
+            classNames={{
+              dropdown: styles.popover,
+              arrow: styles.popoverArrow
+            }}
+          >
+            <Popover.Target>
+              <div
+                className={styles.more}
+                data-testid="blockchain-more-button"
+                onMouseEnter={() => setIsMoreHovered(true)}
+                onMouseLeave={() => setIsMoreHovered(false)}
+              >
+                <span className={styles.moreCount}>+{moreCount}</span>
+              </div>
+            </Popover.Target>
+            <Popover.Dropdown>
+              <div className={styles.popoverContent}>
+                {remainingBlockchains.map((blockchain, index) => (
+                  <div
+                    key={`remaining-${index}`}
+                    className={styles.popoverItem}
+                  >
+                    <Image
+                      src={getIconSrc(blockchain.iconUrl)}
+                      alt={blockchain.name}
+                      className={styles.popoverIcons}
+                      onError={() => handleImageError(blockchain.iconUrl)}
+                    />
+                    <span className={styles.popoverBlockchainsName}>
+                      {blockchain.name}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </Popover.Dropdown>
+          </Popover>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default BlockchainsStack
---
+++ src/components/GameInfoV2/index.tsx
@@ -0,0 +1,156 @@
+import React, { useEffect, useState } from 'react'
+
+import classNames from 'classnames'
+
+import { EditorChoice } from '@/assets/images'
+
+import MetaSection from '../MetaSection'
+import SocialLinks from '../SocialLinks/SocialLinks'
+import Sticker from '../Sticker'
+import styles from './GameInfoV2.module.scss'
+
+interface EditorChoiceType {
+  isEditorChoice: boolean
+  year?: number
+  className?: string
+}
+export interface GameInfoV2Props {
+  title: string
+  info: {
+    developer?: string
+  }
+  version?: string
+  earlyAccess?: boolean
+  playerCount?: string
+  ImageComponent?: React.ReactNode
+  blockchains?: React.ReactNode
+  socialLinks?: {
+    type: string
+    url: string
+    className?: string
+  }[]
+  editorChoice?: EditorChoiceType
+  actionButton?: React.ReactNode
+  isLoading?: boolean
+  className?: string
+  i18n?: {
+    editorChoice?: string
+    earlyAccess?: string
+    developer?: string
+    playerCount?: string
+    version?: string
+  }
+}
+
+const GameInfoV2: React.FC<GameInfoV2Props> = ({
+  title,
+  info,
+  version,
+  earlyAccess,
+  playerCount,
+  ImageComponent,
+  blockchains,
+  socialLinks,
+  editorChoice,
+  actionButton,
+  isLoading,
+  className,
+  i18n = {
+    editorChoice: "Editor's Choice",
+    version: 'Version',
+    earlyAccess: 'Early Access',
+    developer: 'Developer',
+    playerCount: 'Player Count'
+  }
+}): JSX.Element => {
+  const [isImageLoading, setIsImageLoading] = useState(isLoading)
+
+  useEffect(() => {
+    if (ImageComponent && !isImageLoading) {
+      setIsImageLoading(false)
+    }
+    if (!ImageComponent && isImageLoading) {
+      setIsImageLoading(true)
+    }
+  }, [ImageComponent, isImageLoading])
+
+  let editorChoiceElement = null
+  if (editorChoice?.isEditorChoice) {
+    editorChoiceElement = (
+      <div className={classNames(styles.editorChoice, editorChoice.className)}>
+        <EditorChoice />
+        {i18n.editorChoice}
+        {editorChoice.year || new Date().getFullYear()}
+      </div>
+    )
+  }
+
+  return (
+    <div className={classNames(styles.container, className)}>
+      <div className={styles.imageContainer}>
+        {isLoading ? <div className={styles.imageLoading} /> : ImageComponent}
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            {editorChoiceElement}
+            <span className={classNames(styles.title)}>{title}</span>
+          </div>
+
+          <div className={styles.info}>
+            <div className={styles.badges}>
+              <MetaSection
+                title=""
+                items={[
+                  <Sticker
+                    key={i18n.version}
+                    styleType="neutral"
+                    variant="filledStrong"
+                  >
+                    {version}
+                  </Sticker>,
+                  earlyAccess ? (
+                    <Sticker
+                      key={i18n.earlyAccess}
+                      styleType="neutral"
+                      variant="filledStrong"
+                    >
+                      {i18n.earlyAccess}
+                    </Sticker>
+                  ) : null,
+                  <Sticker
+                    key={i18n.developer}
+                    styleType="neutral"
+                    variant="filledStrong"
+                  >
+                    {info.developer}
+                  </Sticker>,
+                  playerCount ? (
+                    <Sticker
+                      key={i18n.playerCount}
+                      styleType="neutral"
+                      variant="filledStrong"
+                    >
+                      {playerCount}
+                    </Sticker>
+                  ) : null
+                ]}
+                maxVisibleItems={8}
+                moreIndicator={<></>}
+              />
+            </div>
+            {blockchains}
+          </div>
+        </div>
+
+        <div className={styles.actions}>
+          {socialLinks && <SocialLinks socialLinks={socialLinks} />}
+          {actionButton}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GameInfoV2
