743d43463c425a4f9f20a6b014e84461f859a061
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-04-09 05:21:33 +0200

    [Chore] update typography variables to match design (#480)

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,75 +1,112 @@
 import React, { HTMLAttributes, PropsWithChildren } from 'react'
 
 export type TypographyProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>>
+export type HeadingProps = PropsWithChildren<HTMLAttributes<HTMLHeadingElement>>
 
-export const Heading1 = (props: TypographyProps) => (
-  <h1 {...props}>{props.children}</h1>
+export const Heading1 = (props: HeadingProps) => (
+  <div {...props} className={`h1 ${props.className ?? ''}`}>
+    {props.children}
+  </div>
 )
 
-export const Heading2 = (props: TypographyProps) => (
-  <h2 {...props}>{props.children}</h2>
+export const Heading2 = (props: HeadingProps) => (
+  <div {...props} className={`h2 ${props.className ?? ''}`}>
+    {props.children}
+  </div>
 )
 
-export const Heading3 = (props: TypographyProps) => (
-  <h3 {...props}>{props.children}</h3>
+export const Heading3 = (props: HeadingProps) => (
+  <div {...props} className={`h3 ${props.className ?? ''}`}>
+    {props.children}
+  </div>
 )
 
-export const Heading4 = (props: TypographyProps) => (
-  <h4 {...props}>{props.children}</h4>
+export const Heading4 = (props: HeadingProps) => (
+  <div {...props} className={`h4 ${props.className ?? ''}`}>
+    {props.children}
+  </div>
 )
 
-export const Heading5 = (props: TypographyProps) => (
-  <h5 {...props}>{props.children}</h5>
+export const Heading5 = (props: HeadingProps) => (
+  <div {...props} className={`h5 ${props.className ?? ''}`}>
+    {props.children}
+  </div>
 )
 
-export const Heading6 = (props: TypographyProps) => (
-  <h6 {...props}>{props.children}</h6>
+export const Heading6 = (props: HeadingProps) => (
+  <div {...props} className={`h6 ${props.className ?? ''}`}>
+    {props.children}
+  </div>
 )
 
 export const Menu = (props: TypographyProps) => (
-  <div {...props} className={`menu ${props.className}`}>
+  <div {...props} className={`menu ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
 
 export const Title = (props: TypographyProps) => (
-  <div {...props} className={`title ${props.className}`}>
+  <div {...props} className={`title ${props.className ?? ''}`}>
+    {props.children}
+  </div>
+)
+
+export const TitleSmall = (props: TypographyProps) => (
+  <div {...props} className={`title-sm ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
 
 export const Caption = (props: TypographyProps) => (
-  <div {...props} className={`caption ${props.className}`}>
+  <div {...props} className={`caption ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
 
 export const CaptionSmall = (props: TypographyProps) => (
-  <div {...props} className={`captionSm ${props.className}`}>
+  <div {...props} className={`caption-sm ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
 
 export const Body = (props: TypographyProps) => (
-  <div {...props} className={`body ${props.className}`}>
+  <div {...props} className={`body ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
 
 export const BodySmall = (props: TypographyProps) => (
-  <div {...props} className={`bodySm ${props.className}`}>
+  <div {...props} className={`body-sm ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
 
-export const ButtonSmallText = (props: TypographyProps) => (
-  <div {...props} className={`buttonSm ${props.className}`}>
+export const ButtonLarge = (props: TypographyProps) => (
+  <div {...props} className={`button-lg ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
 
-export const ButtonText = (props: TypographyProps) => (
-  <div {...props} className={`button ${props.className}`}>
+export const Button = (props: TypographyProps) => (
+  <div {...props} className={`button ${props.className ?? ''}`}>
+    {props.children}
+  </div>
+)
+
+export const Uppercase = (props: TypographyProps) => (
+  <div {...props} className={`uppercase ${props.className ?? ''}`}>
+    {props.children}
+  </div>
+)
+
+export const Eyebrow = (props: TypographyProps) => (
+  <div {...props} className={`eyebrow ${props.className ?? ''}`}>
+    {props.children}
+  </div>
+)
+
+export const EyebrowSmall = (props: TypographyProps) => (
+  <div {...props} className={`eyebrow-sm ${props.className ?? ''}`}>
     {props.children}
   </div>
 )
