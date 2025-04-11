4a1c8022857d1ecbad002180e119eba93f6bcef5
Flavio F Lima <flavioislima@users.noreply.github.com>
2025-04-11 14:16:01 +0100

    chore: cleanup

--- src/components/RewardsSection/constants.ts
+++ src/components/RewardsSection/constants.ts
@@ -5,12 +5,14 @@ export const dummyData: RewardsCardProps[] = [
     id: 1,
     questId: 1,
     rewardImage: '',
-    reward: ''
+    reward: '',
+    rewardType: ''
   },
   {
     id: 2,
     questId: 2,
     rewardImage: '',
-    reward: ''
+    reward: '',
+    rewardType: ''
   }
 ]
