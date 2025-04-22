25333e8c3c674604eab1b3d111af1fdf82f07c3a
Flávio Fearn <26871415+flavioislima@users.noreply.github.com>
2025-04-22 20:35:44 +0100

    [UI] Adds IconsStack component (#499)
    * feat: implement IconsStack component with popover for additional icons
    
    * feat: add wallet icons for MetaMask, Ronin, Coinbase, Trust, Phantom, Sequence, and OKX
    
    * feat: add IconsStack component with default story for wallet icons
    
    * fix: update cursor style for "More" icon in IconsStack component
    
    * chore: style cleanup
    
    * feat: add WithTitleAndMaxThree story for IconsStack
    
    * feat: update version to 1.23.0 and export IconsStack component
    
    * feat: pr fixes and other improvements
    
    * fix: hover glitch
    
    * feat: update version to 1.24.0 in package.json
    
    ---------
    
    Co-authored-by: Flavio F Lima <flavioislima@users.noreply.github.com>

---
+++ src/components/IconsStack/IconsStack.stories.tsx
@@ -0,0 +1,91 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import IconsStack from './index'
+import {
+  MetaMaskWallet,
+  RoninWallet,
+  CoinBaseWallet,
+  TrustWallet,
+  PhantomWallet,
+  SequenceWallet,
+  OKXWallet
+} from '@/assets/images'
+import { expect, userEvent, within } from '@storybook/test'
+
+import storyStyles from './IconsStackStories.module.scss'
+
+const meta = {
+  title: 'Components/IconsStack',
+  component: IconsStack,
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof IconsStack>
+
+export default meta
+
+type Story = StoryObj<typeof IconsStack>
+
+const walletIcons = [
+  { title: 'MetaMask Wallet', icon: <MetaMaskWallet /> },
+  { title: 'Ronin Wallet', icon: <RoninWallet /> },
+  { title: 'Coinbase Wallet', icon: <CoinBaseWallet /> },
+  { title: 'Trust Wallet', icon: <TrustWallet /> },
+  { title: 'Phantom Wallet', icon: <PhantomWallet /> },
+  { title: 'Sequence Wallet', icon: <SequenceWallet /> },
+  { title: 'OKX Wallet', icon: <OKXWallet /> }
+]
+
+export const Default: Story = {
+  args: {
+    title: 'WALLETS:',
+    icons: walletIcons,
+    maxVisible: 5,
+    showMore: true
+  },
+  play: async ({ canvasElement }) => {
+    const moreButton = within(canvasElement)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    await userEvent.hover(moreButton.getByTestId('icons-more-button'), {
+      delay: 800
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await expect(
+      moreButton.getByTestId('icons-more-button')
+    ).toBeInTheDocument()
+
+    await userEvent.unhover(moreButton.getByTestId('icons-more-button'), {
+      delay: 800
+    })
+  }
+}
+
+export const WithCustomClassNames: Story = {
+  args: {
+    title: 'SUPPORTED WALLETS:',
+    icons: walletIcons,
+    maxVisible: 5,
+    showMore: true
+  }
+}
+
+export const WithoutTitle: Story = {
+  args: {
+    icons: walletIcons.slice(0, 3),
+    maxVisible: 3,
+    showMore: false
+  }
+}
+
+export const ForceShowMore: Story = {
+  args: {
+    icons: walletIcons,
+    maxVisible: 7,
+    showMore: true,
+    forceShowMore: true,
+    classNames: {
+      iconsContainer: storyStyles.iconsContainer
+    }
+  }
+}
