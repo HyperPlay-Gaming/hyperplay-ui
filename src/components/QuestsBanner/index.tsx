9e19bad2c4bb1660234ce8759383517aea88e7f8
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2025-05-06 14:54:17 -0700

    fix quests banner for 1 banner item

--- src/components/QuestsBanner/index.tsx
+++ src/components/QuestsBanner/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import { Carousel } from '@mantine/carousel'
 import cn from 'classnames'
 import Autoplay, { AutoplayType } from 'embla-carousel-autoplay'
-import { EmblaCarouselType } from 'embla-carousel-react'
+import { EmblaCarouselType } from 'embla-carousel'
 
 import { Line } from '@/assets/images'
 import Button from '@/components/Button'
@@ -41,7 +41,6 @@ export interface QuestsBannerProps {
     activateIndicator?: string
   }
   list?: QuestsBannerSlideItemProp[]
-  totalPages: number
   canAutoRotate?: boolean
   autoplayDelayInMs?: number
   carousel?: QuestsBannerCarouselWidth
@@ -51,15 +50,18 @@ export interface QuestsBannerProps {
 export const QuestsBanner = ({
   classNames,
   list = [],
-  totalPages,
   canAutoRotate = true,
   autoplayDelayInMs = 6000,
   carousel,
   onPageChangeTap
 }: QuestsBannerProps) => {
   const [currentPage, setCurrentPage] = useState(0)
+  const hasMoreThanOneItem = list.length > 1
   const autoplay = useRef<AutoplayType>(
-    Autoplay({ delay: autoplayDelayInMs, stopOnInteraction: false })
+    Autoplay({
+      delay: autoplayDelayInMs,
+      stopOnInteraction: false
+    })
   )
   const [emblaApiRef, setEmblaApiRef] = useState<EmblaCarouselType>()
 
@@ -70,14 +72,14 @@ export const QuestsBanner = ({
   }
 
   useEffect(() => {
-    if (emblaApiRef) {
+    if (emblaApiRef && hasMoreThanOneItem) {
       if (canAutoRotate) {
         autoplay.current.play()
       } else {
         autoplay.current.stop()
       }
     }
-  }, [emblaApiRef, canAutoRotate])
+  }, [emblaApiRef, canAutoRotate, hasMoreThanOneItem])
 
   return (
     <div
@@ -104,23 +106,25 @@ export const QuestsBanner = ({
       <div className={cn(styles.content, classNames?.content)}>
         <div className={cn(styles.carouselWrapper)}>
           <Carousel
-            getEmblaApi={(embla) => setEmblaApiRef(embla)}
+            getEmblaApi={(embla) =>
+              hasMoreThanOneItem ? setEmblaApiRef(embla) : undefined
+            }
             classNames={{
               slide: cn(styles.slide, classNames?.slide)
             }}
             onSlideChange={(index) => {
               setCurrentPage(index)
             }}
-            plugins={[autoplay.current]}
+            plugins={hasMoreThanOneItem ? [autoplay.current] : []}
             onMouseEnter={() => {
-              if (canAutoRotate) {
-                autoplay.current.stop()
+              if (canAutoRotate && hasMoreThanOneItem) {
+                autoplay.current?.stop()
               }
             }}
             onMouseLeave={() => {
-              if (canAutoRotate) {
-                autoplay.current.stop()
-                autoplay.current.play()
+              if (canAutoRotate && hasMoreThanOneItem) {
+                autoplay.current?.stop()
+                autoplay.current?.play()
               }
             }}
             withControls={false}
@@ -182,7 +186,7 @@ export const QuestsBanner = ({
             classNames?.paginitionContainer
           )}
         >
-          {Array.from({ length: totalPages }).map((_, index) => (
+          {Array.from({ length: list.length }).map((_, index) => (
             <Line
               onClick={() => handlePageChange(index)}
               className={cn(
