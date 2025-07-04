b1de080e52b46921301b8bf6ed9607be2f63c273
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-07-04 20:33:04 +0100

    Refactor MasonryLayout to accept an array of images and improve layout handling based on image count

--- src/components/MasonryLayout/index.tsx
+++ src/components/MasonryLayout/index.tsx
@@ -1,25 +1,81 @@
 import React from 'react'
 import styles from './MasonryLayout.module.scss'
-import gameImage from '@/assets/banners/QuestCardV2Image.png?url'
+
+import classNames from 'classnames'
 
 export interface MasonryLayoutProps {
-  imageUrl?: React.ReactNode
+  images?: React.ReactNode[]
+  imageUrl?: string
 }
 
-const MasonryLayout = ({ imageUrl }: MasonryLayoutProps) => {
-  const image = imageUrl || gameImage
+const MasonryLayout = ({ images }: MasonryLayoutProps) => {
+  if (!images || images.length < 1 || images.length > 5) return null
 
-  return (
-    <div className={styles.masonryGrid}>
-      <div className={styles.largeImage}>{image}</div>
-      <div className={styles.mediumImagesContainer}>
-        <div className={styles.mediumImage}>{image}</div>
-        <div className={styles.mediumImage}>{image}</div>
-        <div className={styles.mediumImage}>{image}</div>
-        <div className={styles.mediumImage}>{image}</div>
-      </div>
-    </div>
-  )
+  const gridClass =
+    images.length === 5
+      ? [styles.masonryGrid, styles['masonryGrid--5']]
+      : images.length === 4
+      ? [styles.masonryGrid, styles['masonryGrid--4']]
+      : images.length === 3
+      ? [styles.masonryGrid, styles['masonryGrid--3']]
+      : images.length === 2
+      ? [styles.masonryGrid, styles['masonryGrid--2']]
+      : images.length === 1
+      ? [styles.masonryGrid, styles['masonryGrid--1']]
+      : [styles.masonryGrid]
+
+  switch (images.length) {
+    case 5:
+      return (
+        <div className={classNames(...gridClass)}>
+          <div className={styles.largeImage}>{images[0]}</div>
+          <div className={styles.smallImagesContainer}>
+            <div className={styles.smallImage}>{images[1]}</div>
+            <div className={styles.smallImage}>{images[2]}</div>
+            <div className={styles.smallImage}>{images[3]}</div>
+            <div className={styles.smallImage}>{images[4]}</div>
+          </div>
+        </div>
+      )
+    case 4:
+      return (
+        <div className={classNames(...gridClass)}>
+          {images.map((img, i) => (
+            <div className={styles.smallImage} key={i}>
+              {img}
+            </div>
+          ))}
+        </div>
+      )
+    case 3:
+      return (
+        <div className={classNames(...gridClass)}>
+          {images.map((img, i) => (
+            <div className={styles.mediumImage} key={i}>
+              {img}
+            </div>
+          ))}
+        </div>
+      )
+    case 2:
+      return (
+        <div className={classNames(...gridClass)}>
+          {images.map((img, i) => (
+            <div className={styles.largeImage} key={i}>
+              {img}
+            </div>
+          ))}
+        </div>
+      )
+    case 1:
+      return (
+        <div className={classNames(...gridClass)}>
+          <div className={styles.largeImage}>{images[0]}</div>
+        </div>
+      )
+    default:
+      return null
+  }
 }
 
 export default MasonryLayout
