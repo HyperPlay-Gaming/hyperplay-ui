091a3f356c26d4b756f5be15cc3fddb878656297
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2025-07-09 14:57:33 -0700

    rm button from use client exclude

--- rollup-exclude-use-client.ts
+++ rollup-exclude-use-client.ts
@@ -1,8 +1,8 @@
 // Files names that should not have use client directive at the top of the output file
-export const ROLLUP_EXCLUDE_USE_CLIENT = [
-  'src/index',
-  'src/components/Button/index'
-].reduce<string[]>((acc, name) => {
-  acc.push(`${name}.js`, `${name}.mjs`, `${name}.cjs`)
-  return acc
-}, [])
+export const ROLLUP_EXCLUDE_USE_CLIENT = ['src/index'].reduce<string[]>(
+  (acc, name) => {
+    acc.push(`${name}.js`, `${name}.mjs`, `${name}.cjs`)
+    return acc
+  },
+  []
+)
